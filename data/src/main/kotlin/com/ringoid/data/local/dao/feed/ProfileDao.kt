4d0008564bd1ae9e40df82964138e51bda2c962b
Maxim Alov <orcchg@yandex.ru>
2018-12-26 00:07:39 +0300

    Get profiles with images from database

--- data/src/main/kotlin/com/ringoid/data/local/dao/feed/ProfileDao.kt
+++ data/src/main/kotlin/com/ringoid/data/local/dao/feed/ProfileDao.kt
@@ -1,6 +1,14 @@
 package com.ringoid.data.local.dao.feed
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.ringoid.data.local.model.feed.ProfileDbo
+import com.ringoid.data.local.model.feed.ProfileWithImagesDbo
+import io.reactivex.Observable
 
 @Dao
-interface ProfileDao
+interface ProfileDao {
+
+    @Query("SELECT * FROM ${ProfileDbo.TABLE_NAME}")
+    fun profiles(): Observable<List<ProfileWithImagesDbo>>
+}
