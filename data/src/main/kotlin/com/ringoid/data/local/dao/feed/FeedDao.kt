fbba3e4c3d9bac7114059ba861835219166dccfd
Maxim Alov <orcchg@yandex.ru>
2018-12-26 00:39:13 +0300

    Add profiles, profile images and user images to database

--- data/src/main/kotlin/com/ringoid/data/local/dao/feed/FeedDao.kt
+++ data/src/main/kotlin/com/ringoid/data/local/dao/feed/FeedDao.kt
@@ -1,9 +1,12 @@
 package com.ringoid.data.local.dao.feed
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ringoid.data.local.model.feed.ProfileDbo
 import com.ringoid.data.local.model.feed.ProfileWithImagesDbo
+import com.ringoid.data.local.model.image.ImageDbo
 import io.reactivex.Observable
 
 @Dao
@@ -11,4 +14,10 @@ interface FeedDao {
 
     @Query("SELECT * FROM ${ProfileDbo.TABLE_NAME}")
     fun profiles(): Observable<List<ProfileWithImagesDbo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addProfiles(profiles: Collection<ProfileDbo>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addProfileImages(images: Collection<ImageDbo>)
 }
--- data/src/main/kotlin/com/ringoid/data/local/dao/image/ImageDao.kt
+++ data/src/main/kotlin/com/ringoid/data/local/dao/image/ImageDao.kt
@@ -1,6 +1,8 @@
 package com.ringoid.data.local.dao.image
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ringoid.data.local.model.image.UserImageDbo
 import io.reactivex.Observable
@@ -10,4 +12,7 @@ interface ImageDao {
 
     @Query("SELECT * FROM ${UserImageDbo.TABLE_NAME}")
     fun userImages(): Observable<List<UserImageDbo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addUserImages(images: Collection<UserImageDbo>)
 }
