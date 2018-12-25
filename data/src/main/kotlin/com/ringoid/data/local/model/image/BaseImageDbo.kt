8b89ed47924140576f267526ba88f61a44661757
Maxim Alov <orcchg@yandex.ru>
2018-12-26 00:00:30 +0300

    Renamed url field to uri

--- data/src/main/kotlin/com/ringoid/data/local/model/image/BaseImageDbo.kt
+++ data/src/main/kotlin/com/ringoid/data/local/model/image/BaseImageDbo.kt
@@ -10,12 +10,12 @@ import com.ringoid.data.local.model.feed.ProfileDbo
 data class BaseImageDbo(
     @PrimaryKey @ColumnInfo(name = COLUMN_ID) val id: Int,
     @ColumnInfo(name = COLUMN_PROFILE_ID) val profileId: String,
-    @ColumnInfo(name = COLUMN_URL) val url: String) {
+    @ColumnInfo(name = COLUMN_URI) val uri: String) {
 
     companion object {
         const val COLUMN_ID = "id"
         const val COLUMN_PROFILE_ID = "profileId"
-        const val COLUMN_URL = "url"
+        const val COLUMN_URI = "uri"
 
         const val TABLE_NAME = "Images"
     }
