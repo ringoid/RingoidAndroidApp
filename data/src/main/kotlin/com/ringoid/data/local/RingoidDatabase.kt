5372096050c7216edadef64a578d944d161299e9
Maxim Alov <orcchg@yandex.ru>
2018-12-26 00:04:27 +0300

    Added stub for profile dao

--- data/src/main/kotlin/com/ringoid/data/local/RingoidDatabase.kt
+++ data/src/main/kotlin/com/ringoid/data/local/RingoidDatabase.kt
@@ -2,8 +2,12 @@ package com.ringoid.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ringoid.data.local.dao.feed.ProfileDao
+import com.ringoid.data.local.model.feed.ProfileDbo
+import com.ringoid.data.local.model.image.BaseImageDbo
 
-@Database(entities = [], version = 1)
+@Database(entities = [BaseImageDbo::class, ProfileDbo::class], version = 1)
 abstract class RingoidDatabase : RoomDatabase() {
 
+    abstract fun profileDao(): ProfileDao
 }
---
+++ data/src/main/kotlin/com/ringoid/data/local/dao/feed/ProfileDao.kt
@@ -0,0 +1,6 @@
+package com.ringoid.data.local.dao.feed
+
+import androidx.room.Dao
+
+@Dao
+interface ProfileDao
