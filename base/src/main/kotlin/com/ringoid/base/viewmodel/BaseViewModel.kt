0b6aba6fb0869207fb50e80511db398859798632
Maxim Alov <orcchg@yandex.ru>
2018-12-27 10:45:55 +0300

    Added base view-model

---
+++ base/src/main/kotlin/com/ringoid/base/viewmodel/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.ringoid.base.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+abstract class BaseViewModel(app: Application) : AndroidViewModel(app)
