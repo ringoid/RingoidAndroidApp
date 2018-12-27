73afaefdf4eb4f8529d5218008a01c7a8278f9ec
Maxim Alov <orcchg@yandex.ru>
2018-12-27 21:19:05 +0300

    ViewModel should dispose if it is subscribed and destroyed

--- base/src/main/kotlin/com/ringoid/base/viewmodel/BaseViewModel.kt
+++ base/src/main/kotlin/com/ringoid/base/viewmodel/BaseViewModel.kt
@@ -4,8 +4,20 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.ringoid.base.view.ViewState
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 abstract class BaseViewModel(app: Application) : AndroidViewModel(app) {
 
+    protected var subs: Disposable? = null  // for single subscription
+    protected val cs: CompositeDisposable = CompositeDisposable()  // for multiple subscriptions
+
     val viewState: MutableLiveData<ViewState> by lazy { MutableLiveData<ViewState>(ViewState.IDLE) }
+
+    override fun onCleared() {
+        super.onCleared()
+        subs?.dispose()
+        subs = null
+        cs.clear()
+    }
 }
--- origin/src/main/kotlin/com/ringoid/origin/view/auth/LoginViewModel.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/auth/LoginViewModel.kt
@@ -1,15 +1,24 @@
 package com.ringoid.origin.view.auth
 
 import android.app.Application
+import android.os.Build
 import androidx.lifecycle.MutableLiveData
+import com.ringoid.base.view.ViewState
 import com.ringoid.base.viewmodel.BaseViewModel
+import com.ringoid.domain.interactor.base.Params
+import com.ringoid.domain.interactor.user.CreateUserProfileUseCase
 import com.ringoid.domain.misc.Gender
+import com.ringoid.domain.model.essence.user.AuthCreateProfileEssence
 import com.ringoid.origin.RingoidApplication
 import com.ringoid.utility.isAdultAge
 import com.ringoid.widget.WidgetState
+import timber.log.Timber
+import java.util.*
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor(app: Application) : BaseViewModel(app) {
+class LoginViewModel @Inject constructor(
+    private val createUserProfileUseCase: CreateUserProfileUseCase,
+    app: Application) : BaseViewModel(app) {
 
     val calendar by lazy { getApplication<RingoidApplication>().calendar }
 
@@ -25,7 +34,22 @@ class LoginViewModel @Inject constructor(app: Application) : BaseViewModel(app) 
 
     // --------------------------------------------------------------------------------------------
     fun login() {
-        // TODO: get year and gender
+        val essence = AuthCreateProfileEssence(
+            yearOfBirth = yearOfBirth,
+            dtLegalAge = System.currentTimeMillis(),
+            locale = Locale.getDefault().language,
+            sex = gender?.string ?: Gender.MALE.string /* safe null-check */,
+            device = String.format("%s, %d", Build.VERSION.RELEASE, Build.VERSION.SDK_INT),
+            osVersion = String.format("%s, %s, %s", Build.MODEL, Build.MANUFACTURER, Build.PRODUCT))
+
+        subs = createUserProfileUseCase.source(params = Params().put(essence))
+            .doOnSubscribe { viewState.value = ViewState.LOADING }
+            .doOnSuccess { viewState.value = ViewState.IDLE }
+            .doOnError { viewState.value = ViewState.ERROR(it) }
+            .subscribe({
+                Timber.d("Successfully logged in, current user: $it")
+                // TODO: proceed further screen
+            }, Timber::e)
     }
 
     fun onGenderSelect(gender: Gender) {
