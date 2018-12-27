b350094d919bc66d0baa868fe1d82e7854feea2e
Maxim Alov <orcchg@yandex.ru>
2018-12-27 10:48:55 +0300

    Added stub for Login screen vm

---
+++ origin/src/main/kotlin/com/ringoid/origin/view/auth/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.ringoid.origin.view.auth
+
+import android.app.Application
+import com.ringoid.base.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+class LoginViewModel @Inject constructor(app: Application) : BaseViewModel(app) {
+
+}
