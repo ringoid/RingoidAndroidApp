c5b6a22157fa1305aa7526bb69d42e2b12685880
Maxim Alov <orcchg@yandex.ru>
2018-12-27 18:15:21 +0300

    Handle gender selections on Login screen

--- origin/src/main/kotlin/com/ringoid/origin/view/auth/LoginActivity.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/auth/LoginActivity.kt
@@ -6,6 +6,7 @@ import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.widget.textChanges
 import com.ringoid.base.view.BaseActivity
 import com.ringoid.base.view.ViewState
+import com.ringoid.domain.misc.Gender
 import com.ringoid.origin.R
 import com.ringoid.utility.changeVisibility
 import com.ringoid.utility.clickDebounce
@@ -43,15 +44,27 @@ class LoginActivity : BaseActivity<LoginViewModel>() {
         super.onCreate(savedInstanceState)
         btn_login.clicks().compose(clickDebounce()).subscribe { vm.login() }
         et_year_of_birth.textChanges().compose(inputDebounce()).subscribe { vm.onYearOfBirthChange(it.toString()) }
+        tv_sex_male.clicks().compose(clickDebounce()).subscribe {
+            tv_sex_male.takeIf { it.isSelected } ?: run {
+                tv_sex_male.isSelected = true
+                tv_sex_female.isSelected = false
+                vm.onGenderSelect(Gender.MALE)
+            }
+        }
+        tv_sex_female.clicks().compose(clickDebounce()).subscribe {
+            tv_sex_female.takeIf { it.isSelected } ?: run {
+                tv_sex_male.isSelected = false
+                tv_sex_female.isSelected = true
+                vm.onGenderSelect(Gender.FEMALE)
+            }
+        }
 
         vm.loginButtonEnableState.observe(this, Observer { btn_login.isEnabled = it })
         vm.yearOfBirthEntryState.observe(this, Observer {
             when (it) {
                 WidgetState.NORMAL -> {
                     et_year_of_birth.setBackgroundResource(R.drawable.rect_round_grey)
-                    iv_status.also {
-                        it.changeVisibility(isVisible = false)
-                    }
+                    iv_status.also { it.changeVisibility(isVisible = false) }
                 }
                 WidgetState.ACTIVE -> {
                     et_year_of_birth.setBackgroundResource(R.drawable.rect_round_green)
