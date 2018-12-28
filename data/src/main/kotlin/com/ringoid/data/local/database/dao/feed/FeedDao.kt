1d0e16562ac6c3d3ccb65a2fb2266b838a1a3496
Maxim Alov <orcchg@yandex.ru>
2018-12-28 20:49:34 +0300

    Query related fields - profile and it's images - in one single transaction instead of in separate queries by default

--- data/src/main/kotlin/com/ringoid/data/local/database/dao/feed/FeedDao.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/dao/feed/FeedDao.kt
@@ -1,9 +1,6 @@
 package com.ringoid.data.local.database.dao.feed
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.ringoid.data.local.database.model.feed.ProfileDbo
 import com.ringoid.data.local.database.model.feed.ProfileWithImagesDbo
 import com.ringoid.data.local.database.model.image.ImageDbo
@@ -12,6 +9,7 @@ import io.reactivex.Observable
 @Dao
 interface FeedDao {
 
+    @Transaction
     @Query("SELECT * FROM ${ProfileDbo.TABLE_NAME}")
     fun profiles(): Observable<List<ProfileWithImagesDbo>>
 
