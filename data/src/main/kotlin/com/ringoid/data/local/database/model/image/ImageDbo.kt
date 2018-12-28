d54dc1376792576311778856d2462164412831ed
Maxim Alov <orcchg@yandex.ru>
2018-12-28 20:10:10 +0300

    Try to avoid warning with non-indexed foreign key column

--- data/src/main/kotlin/com/ringoid/data/local/database/model/image/ImageDbo.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/model/image/ImageDbo.kt
@@ -3,15 +3,14 @@ package com.ringoid.data.local.database.model.image
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.Index
 import com.ringoid.data.local.database.model.feed.ProfileDbo
 
-@Entity(tableName = ImageDbo.TABLE_NAME, indices = [Index(value = [ProfileDbo.COLUMN_ID], name = "imageDboIndex")],
+@Entity(tableName = ImageDbo.TABLE_NAME,
         foreignKeys = [ForeignKey(entity = ProfileDbo::class, parentColumns = [ProfileDbo.COLUMN_ID],
                                   childColumns = [ImageDbo.COLUMN_PROFILE_ID],
                                   onDelete = ForeignKey.CASCADE, onUpdate = ForeignKey.CASCADE)])
 class ImageDbo(
-    @ColumnInfo(name = COLUMN_PROFILE_ID) val profileId: String,
+    @ColumnInfo(name = COLUMN_PROFILE_ID, index = true) val profileId: String,
     id: String, uri: String) : BaseImageDbo(id = id, uri = uri) {
 
     companion object {
