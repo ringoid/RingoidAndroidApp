e342053d80a59e647d0e94e3501152d17169ec81
Maxim Alov macbook <orcchg@gmail.com>
2019-01-01 17:47:57 +0300

    Fixed annotation processsing for abstract class view model

--- origin/src/main/kotlin/com/ringoid/origin/view/main/BaseMainViewModel.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/main/BaseMainViewModel.kt
@@ -2,6 +2,5 @@ package com.ringoid.origin.view.main
 
 import android.app.Application
 import com.ringoid.base.viewmodel.BaseViewModel
-import javax.inject.Inject
 
-abstract class BaseMainViewModel @Inject constructor(app: Application) : BaseViewModel(app)
+abstract class BaseMainViewModel(app: Application) : BaseViewModel(app)
