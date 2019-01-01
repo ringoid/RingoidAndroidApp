0f04fb22517755091134c5cd1a9aca1fbb6f0d3e
Maxim Alov macbook <orcchg@gmail.com>
2019-01-01 12:06:49 +0300

    Added instantiation method for Profile screen fragment

--- profile/src/main/java/com/ringoid/origin/profile/view/ProfileFragment.kt
+++ profile/src/main/java/com/ringoid/origin/profile/view/ProfileFragment.kt
@@ -2,4 +2,11 @@ package com.ringoid.origin.profile.view
 
 import com.ringoid.base.view.BaseFragment
 
-class ProfileFragment : BaseFragment()
+class ProfileFragment : BaseFragment() {
+
+    companion object {
+        const val TAG = "ProfileFragment_tag"
+
+        fun newInstance(): ProfileFragment = ProfileFragment()
+    }
+}
