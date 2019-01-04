d42d7958a6752f15c343c8cc068fead3d96ca02c
Maxim Alov macbook <orcchg@gmail.com>
2019-01-04 12:53:17 +0300

    Rely on Fragment's View lifecycle when observing for LiveData - so LiveData gets unsubscribed when view is destroyed, so avoid multiple identical observers being added to the same lifecycle, because non-Fragment's lifecycle is using and the Fragment is safe to be detached and reattached without it's instance being destroyed

--- base/src/main/kotlin/com/ringoid/base/view/BaseFragment.kt
+++ base/src/main/kotlin/com/ringoid/base/view/BaseFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import com.ringoid.base.observe
 import com.ringoid.base.viewModel
@@ -13,9 +14,6 @@ import com.ringoid.base.viewmodel.BaseViewModel
 import timber.log.Timber
 import javax.inject.Inject
 
-/**
- * @see https://medium.com/@BladeCoder/architecture-components-pitfalls-part-1-9300dd969808
- */
 abstract class BaseFragment<T : BaseViewModel> : Fragment() {
 
     protected lateinit var vm: T
@@ -33,19 +31,25 @@ abstract class BaseFragment<T : BaseViewModel> : Fragment() {
 
     /* Lifecycle */
     // --------------------------------------------------------------------------------------------
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(getLayoutId(), container, false)
     }
 
+    /**
+     * Follow the current view hierarchy lifecycle rather than [Fragment]'s lifecycle in order to
+     * unsubscribe observers of [LiveData], which has subscribed in [Fragment.onCreateView] safely
+     * in [Fragment.onDestroyView]. This avoids double-subscription here in [Fragment.onActivityCreated]
+     * for the [Fragment]s that detach and then reattach, because in that case lifespan of [Fragment]
+     * is longer than lifespan of it's view hierarchy. So the observer is added to [Fragment.getViewLifecycleOwner].
+     *
+     * @see https://medium.com/@BladeCoder/architecture-components-pitfalls-part-1-9300dd969808
+     */
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         vm = viewModel(klass = getVmClass(), factory = vmFactory) {
-            observe(viewState) { onViewStateChange(it) }
+            // tie observer to view's lifecycle rather than Fragment's one
+            viewLifecycleOwner.observe(viewState) { onViewStateChange(it) }
         }
     }
 }
