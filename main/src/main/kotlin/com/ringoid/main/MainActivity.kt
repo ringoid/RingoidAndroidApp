58ab87a6329594d43dc950a06769fde693273da1
Maxim Alov macbook <orcchg@gmail.com>
2019-01-04 13:40:36 +0300

    Moved frag-nav-controller initialization login to base class

--- main/src/main/kotlin/com/ringoid/main/MainActivity.kt
+++ main/src/main/kotlin/com/ringoid/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.ringoid.main
 
-import android.os.Bundle
-import com.ncapdevi.fragnav.FragNavController
-import com.ringoid.origin.R
+import androidx.fragment.app.Fragment
 import com.ringoid.origin.profile.view.profile.ProfileFragment
 import com.ringoid.origin.view.feed.explore.ExploreFragment
 import com.ringoid.origin.view.feed.lmm.LmmFragment
@@ -10,26 +8,14 @@ import com.ringoid.origin.view.main.BaseMainActivity
 
 class MainActivity : BaseMainActivity<MainViewModel>() {
 
-    private lateinit var fragNav: FragNavController
-
     override fun getVmClass() = MainViewModel::class.java
+
+    override fun getListOfRootFragment(): List<Fragment> =
+            listOf(
+                ExploreFragment.newInstance(),
+                LmmFragment.newInstance(),
+                ProfileFragment.newInstance())
 
     /* Lifecycle */
     // --------------------------------------------------------------------------------------------
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        fragNav = FragNavController(supportFragmentManager, R.id.fl_container)
-            .apply {
-                rootFragments = listOf(
-                    ExploreFragment.newInstance(),
-                    LmmFragment.newInstance(),
-                    ProfileFragment.newInstance())
-                initialize(index = FragNavController.TAB1, savedInstanceState = savedInstanceState)
-            }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        fragNav.onSaveInstanceState(outState)
-    }
 }
--- origin/src/main/kotlin/com/ringoid/origin/view/main/BaseMainActivity.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/main/BaseMainActivity.kt
@@ -1,12 +1,43 @@
 package com.ringoid.origin.view.main
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ncapdevi.fragnav.FragNavController
+import com.ncapdevi.fragnav.FragNavSwitchController
+import com.ncapdevi.fragnav.FragNavTransactionOptions
+import com.ncapdevi.fragnav.tabhistory.UnlimitedTabHistoryStrategy
 import com.ringoid.base.view.BaseActivity
 import com.ringoid.origin.R
+import com.ringoid.origin.view.feed.explore.ExploreFragment
+import com.ringoid.origin.view.feed.lmm.LmmFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 abstract class BaseMainActivity<VM : BaseMainViewModel> : BaseActivity<VM>() {
 
+    protected lateinit var fragNav: FragNavController
+
     override fun getLayoutId(): Int = R.layout.activity_main
+
+    protected abstract fun getListOfRootFragment(): List<Fragment>
 
     /* Lifecycle */
     // --------------------------------------------------------------------------------------------
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragNav = FragNavController(supportFragmentManager, R.id.fl_container)
+            .apply {
+                rootFragments = getListOfRootFragment()
+                navigationStrategy = UnlimitedTabHistoryStrategy(object : FragNavSwitchController {
+                    override fun switchTab(index: Int, transactionOptions: FragNavTransactionOptions?) {
+                        bottom_bar.selectedItemId
+                    }
+                })
+                initialize(index = FragNavController.TAB1, savedInstanceState = savedInstanceState)
+            }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        fragNav.onSaveInstanceState(outState)
+    }
 }
