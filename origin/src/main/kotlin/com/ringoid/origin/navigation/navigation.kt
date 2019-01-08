1852e7e5a335d045510266773e02fdde5c282a72
Maxim Alov macbook <orcchg@gmail.com>
2019-01-08 13:59:29 +0300

    Log naigation payload

--- origin/src/main/kotlin/com/ringoid/origin/navigation/navigation.kt
+++ origin/src/main/kotlin/com/ringoid/origin/navigation/navigation.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import androidx.fragment.app.Fragment
 import com.ringoid.domain.BuildConfig
+import timber.log.Timber
 
 fun navigate(path: String): Intent =
     Intent(Intent.ACTION_VIEW, Uri.parse("${BuildConfig.APPNAV}$path"))
 
 fun navigate(activity: Activity, path: String, rc: Int = 0, data: Intent? = null) {
+    Timber.v("navigate: path=$path, rc=$rc, data=$data")
     val intent = navigate(path).apply { data?.let { putExtras(it) ; setData(it.data) } }
     rc.takeIf { it != 0 }
       ?.let { activity.startActivityForResult(intent, rc) }
