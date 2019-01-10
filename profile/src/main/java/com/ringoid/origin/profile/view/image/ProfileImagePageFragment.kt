3ad278c2461659bfc6d3e06cffb877a7239621af
Maxim Alov <orcchg@yandex.ru>
2019-01-10 17:48:27 +0300

    Update ProfileImagePage view state on idle, loading and error

--- profile/src/main/java/com/ringoid/origin/profile/view/image/ProfileImagePageFragment.kt
+++ profile/src/main/java/com/ringoid/origin/profile/view/image/ProfileImagePageFragment.kt
@@ -3,9 +3,12 @@ package com.ringoid.origin.profile.view.image
 import android.os.Bundle
 import android.view.View
 import com.jakewharton.rxbinding3.view.clicks
+import com.ringoid.base.view.ViewState
 import com.ringoid.domain.model.image.Image
 import com.ringoid.origin.profile.R
+import com.ringoid.origin.view.dialog.Dialogs
 import com.ringoid.origin.view.image.ImagePageFragment
+import com.ringoid.utility.changeVisibility
 import com.ringoid.utility.clickDebounce
 import kotlinx.android.synthetic.main.fragment_profile_image_page.*
 
@@ -22,6 +25,25 @@ class ProfileImagePageFragment : ImagePageFragment<ProfileImagePageViewModel>() 
 
     override fun getLayoutId(): Int = R.layout.fragment_profile_image_page
 
+    // --------------------------------------------------------------------------------------------
+    override fun onViewStateChange(newState: ViewState) {
+        fun onIdleState() {
+            pb_image.changeVisibility(isVisible = false)
+        }
+
+        super.onViewStateChange(newState)
+        when (newState) {
+            is ViewState.IDLE -> onIdleState()
+            is ViewState.LOADING -> pb_image.changeVisibility(isVisible = true)
+            is ViewState.DONE -> {}  // TODO: notify image removed
+            is ViewState.ERROR -> {
+                // TODO: analyze: newState.e
+                Dialogs.showTextDialog(activity, titleResId = R.string.error_common, description = "DL TEXT FROM URL")
+                onIdleState()
+            }
+        }
+    }
+    
     /* Lifecycle */
     // --------------------------------------------------------------------------------------------
     @Suppress("CheckResult", "AutoDispose")
