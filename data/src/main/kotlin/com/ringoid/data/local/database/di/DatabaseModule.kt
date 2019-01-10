9708f52a02aca404dd398597cdb3246dcb88e67e
Maxim Alov <orcchg@yandex.ru>
2019-01-10 18:26:35 +0300

    Provide dao to Dagger

--- data/src/main/kotlin/com/ringoid/data/local/database/di/DatabaseModule.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/di/DatabaseModule.kt
@@ -3,7 +3,9 @@ package com.ringoid.data.local.database.di
 import android.content.Context
 import androidx.room.Room
 import com.ringoid.data.local.database.RingoidDatabase
+import com.ringoid.data.local.database.dao.feed.FeedDao
 import com.ringoid.data.local.database.dao.image.ImageDao
+import com.ringoid.data.local.database.dao.messenger.MessageDao
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -18,5 +20,11 @@ class DatabaseModule {
             .build()
 
     @Provides @Singleton
+    fun provideFeedDao(database: RingoidDatabase): FeedDao = database.feedDao()
+
+    @Provides @Singleton
     fun provideImageDao(database: RingoidDatabase): ImageDao = database.imageDao()
+
+    @Provides @Singleton
+    fun provideMessageDao(database: RingoidDatabase): MessageDao = database.messageDao()
 }
