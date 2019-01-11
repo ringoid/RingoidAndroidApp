24cbca2c54dfa969de5c71b747dedbb46d149f26
Maxim Alov <orcchg@yandex.ru>
2019-01-11 21:32:20 +0300

    Style

--- profile/src/main/java/com/ringoid/origin/profile/view/profile/ProfileImagePagerAdapter.kt
+++ profile/src/main/java/com/ringoid/origin/profile/view/profile/ProfileImagePagerAdapter.kt
@@ -2,7 +2,6 @@ package com.ringoid.origin.profile.view.profile
 
 import androidx.fragment.app.FragmentManager
 import com.ringoid.domain.model.image.IImage
-import com.ringoid.domain.model.image.Image
 import com.ringoid.origin.profile.view.image.ProfileImagePageFragment
 import com.ringoid.origin.view.adapter.ImagePagerAdapter
 import com.ringoid.origin.view.common.EmptyFragment
@@ -10,6 +9,5 @@ import com.ringoid.origin.view.image.ImagePageFragment
 
 class ProfileImagePagerAdapter(fm: FragmentManager, emptyInput: EmptyFragment.Companion.Input) : ImagePagerAdapter(fm, emptyInput) {
 
-    override fun createImagePageFragment(image: IImage): ImagePageFragment<*> =
-            ProfileImagePageFragment.newInstance(image)
+    override fun createImagePageFragment(image: IImage): ImagePageFragment<*> = ProfileImagePageFragment.newInstance(image)
 }
