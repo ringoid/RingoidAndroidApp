13aa13db6dcbc1bd351e1e4793f733cbefa06a2b
Maxim Alov <orcchg@yandex.ru>
2019-01-11 23:17:42 +0300

    Add loading on image as placeholder

--- utility/src/main/kotlin/com/ringoid/utility/ImageLoader.kt
+++ utility/src/main/kotlin/com/ringoid/utility/ImageLoader.kt
@@ -1,12 +1,24 @@
 package com.ringoid.utility
 
 import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 object ImageLoader {
 
     fun load(uri: String, imageView: ImageView) {
+        val progress = CircularProgressDrawable(imageView.context)
+            .apply {
+                strokeWidth = 5f
+                centerRadius = 30f
+                start()
+            }
+
         // TODO: add retry logic
-        Glide.with(imageView).load(uri).into(imageView)
+        Glide.with(imageView)
+            .load(uri)
+            .apply(RequestOptions().placeholder(progress))
+            .into(imageView)
     }
 }
