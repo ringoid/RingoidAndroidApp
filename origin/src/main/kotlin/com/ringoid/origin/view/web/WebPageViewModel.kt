1aeb1dd91937f9f1384ccbca71e4407e30017aa6
Maxim Alov <orcchg@yandex.ru>
2019-01-14 14:21:25 +0300

    Fix provide web page viewModel to Dagger

--- origin/src/main/kotlin/com/ringoid/origin/view/web/WebPageViewModel.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/web/WebPageViewModel.kt
@@ -2,5 +2,6 @@ package com.ringoid.origin.view.web
 
 import android.app.Application
 import com.ringoid.base.viewmodel.BaseViewModel
+import javax.inject.Inject
 
-class WebPageViewModel(app: Application) : BaseViewModel(app)
+class WebPageViewModel @Inject constructor(app: Application) : BaseViewModel(app)
