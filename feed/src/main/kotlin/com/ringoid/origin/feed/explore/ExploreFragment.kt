2a86525cbcfde8a04fa0db6eb10853faafdd6dac
Maxim Alov <orcchg@yandex.ru>
2019-01-15 19:04:35 +0300

    Hide swipe-refresh loading on idle state, show on loading state

--- feed/src/main/kotlin/com/ringoid/origin/feed/explore/ExploreFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/explore/ExploreFragment.kt
@@ -4,6 +4,7 @@ import com.ringoid.base.view.ViewState
 import com.ringoid.origin.R
 import com.ringoid.origin.feed.FeedFragment
 import com.ringoid.origin.view.dialog.Dialogs
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class ExploreFragment : FeedFragment<ExploreViewModel>() {
 
@@ -16,13 +17,13 @@ class ExploreFragment : FeedFragment<ExploreViewModel>() {
     // --------------------------------------------------------------------------------------------
     override fun onViewStateChange(newState: ViewState) {
         fun onIdleState() {
-            //
+            swipe_refresh_layout.isRefreshing = false
         }
 
         super.onViewStateChange(newState)
         when (newState) {
             is ViewState.IDLE -> onIdleState()
-            is ViewState.LOADING -> {}
+            is ViewState.LOADING -> swipe_refresh_layout.isRefreshing = true
             is ViewState.ERROR -> {
                 // TODO: analyze: newState.e
                 Dialogs.showTextDialog(activity, titleResId = R.string.error_common, description = "DL TEXT FROM URL")
