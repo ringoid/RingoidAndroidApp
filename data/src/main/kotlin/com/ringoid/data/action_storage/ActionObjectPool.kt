fbf9a8d6b882eeeccb90d3c67250b0059944655e
Maxim Alov <orcchg@yandex.ru>
2019-01-16 18:03:11 +0300

    Fix incrementing number of strategies of the same action object type

--- data/src/main/kotlin/com/ringoid/data/action_storage/ActionObjectPool.kt
+++ data/src/main/kotlin/com/ringoid/data/action_storage/ActionObjectPool.kt
@@ -47,7 +47,7 @@ class ActionObjectPool @Inject constructor(
         aobj.javaClass.let { key ->
             numbers.takeIf { !it.containsKey(key) }
                 ?.let { it[key] = 1 }  // first object of that type
-                ?: run { numbers[key]!!.plus(1) }
+                ?: run { numbers[key] = numbers[key]!! + 1 }
 
             /**
              * Check previous [CountFromLast] strategy for [aobj], if any. That strategy relies on
