55162adeddf8bcf6c837bb2a63ddbad90f77be10
Maxim Alov <orcchg@yandex.ru>
2019-01-16 17:01:27 +0300

    Fix exclusion of non-expose fields from (de)serialization by gson

--- domain/src/main/kotlin/com/ringoid/domain/model/IEssence.kt
+++ domain/src/main/kotlin/com/ringoid/domain/model/IEssence.kt
@@ -1,9 +1,15 @@
 package com.ringoid.domain.model
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 
 interface IEssence {
 
-    fun toJson(): String = Gson().toJson(this)
+    fun toJson(): String =
+        GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+            .toJson(this)
+
     fun toJson(gson: Gson): String = gson.toJson(this)
 }
