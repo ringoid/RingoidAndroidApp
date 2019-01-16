8216c9242b95459a820098decff81e7193cecb98
Maxim Alov <orcchg@yandex.ru>
2019-01-16 10:58:11 +0300

    Start implementing ActionObject pool

--- data/src/main/kotlin/com/ringoid/data/action_storage/ActionObjectPool.kt
+++ data/src/main/kotlin/com/ringoid/data/action_storage/ActionObjectPool.kt
@@ -1,8 +1,19 @@
 package com.ringoid.data.action_storage
 
 import com.ringoid.domain.action_storage.IActionObjectPool
+import com.ringoid.domain.model.actions.ActionObject
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ActionObjectPool @Inject constructor() : IActionObjectPool
+class ActionObjectPool @Inject constructor() : IActionObjectPool {
+
+    private val queue: Queue<ActionObject> = ConcurrentLinkedQueue()
+
+    fun put(aobj: ActionObject) {
+        queue.offer(aobj)
+        // TODO
+    }
+}
