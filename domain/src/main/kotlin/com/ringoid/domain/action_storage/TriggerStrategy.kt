674c1d8849f2c56961cf629ced56b6959805f90e
Maxim Alov <orcchg@yandex.ru>
2019-01-16 16:56:37 +0300

    Better toString for trigger strategy classes

--- domain/src/main/kotlin/com/ringoid/domain/action_storage/TriggerStrategy.kt
+++ domain/src/main/kotlin/com/ringoid/domain/action_storage/TriggerStrategy.kt
@@ -1,10 +1,21 @@
 package com.ringoid.domain.action_storage
 
-sealed class TriggerStrategy
+sealed class TriggerStrategy {
+
+    override fun toString(): String = javaClass.simpleName
+}
 
 object Immediate : TriggerStrategy()
-class CountFromLast(val count: Int = LIKE_COUNT_ON_TRIGGER) : TriggerStrategy()
-class DelayFromLast(val delay: Long = LIKE_DELAY_ON_TRIGGER) : TriggerStrategy()  // in seconds
+
+class CountFromLast(val count: Int = LIKE_COUNT_ON_TRIGGER) : TriggerStrategy() {
+
+    override fun toString(): String = "${super.toString()}(count=$count)"
+}
+
+class DelayFromLast(val delay: Long = LIKE_DELAY_ON_TRIGGER) : TriggerStrategy() {  // in seconds
+
+    override fun toString(): String = "${super.toString()}(delay=$delay)"
+}
 
 const val LIKE_COUNT_ON_TRIGGER = 5
 const val LIKE_DELAY_ON_TRIGGER = 5L  // in seconds
