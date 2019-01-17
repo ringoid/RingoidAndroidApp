cd63419d0ee8e2c15b93cfcc6ffa9050dc1375e8
Maxim Alov <orcchg@yandex.ru>
2019-01-17 20:30:13 +0300

    Disable user gesture swipe for view-pager on Lmm screen

---
+++ widget/src/main/kotlin/com/ringoid/widget/view/DisableSwipeViewPager.kt
@@ -0,0 +1,25 @@
+package com.ringoid.widget.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+/**
+ * @see https://stackoverflow.com/questions/29442216/how-to-disable-or-enable-viewpager-swiping-in-android/42687474
+ * @see https://www.oodlestechnologies.com/blogs/Stop-Swipe-Action-in-Android-Viewpager
+ */
+class DisableSwipeViewPager : ViewPager {
+
+    private var disable: Boolean = false
+
+    constructor(context: Context): this(context, null)
+
+    constructor(context: Context, attributes: AttributeSet?): super(context, attributes)
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean =
+        if (disable) false else super.onInterceptTouchEvent(event)
+
+    override fun onTouchEvent(event: MotionEvent): Boolean =
+        if (disable) false else super.onTouchEvent(event)
+}
