fd00de8cf049a5d67e8bc163743a874f683cfb7b
Maxim Alov <orcchg@yandex.ru>
2019-01-17 20:57:00 +0300

    Set layout for message button on Matches item feed programmatically when like button has gone

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/match/MatchFeedAdapter.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/match/MatchFeedAdapter.kt
@@ -1,14 +1,18 @@
 package com.ringoid.origin.feed.adapter.lmm.match
 
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.ringoid.origin.feed.adapter.ProfileViewHolder
 import com.ringoid.origin.feed.adapter.lmm.like.LikeFeedAdapter
+import kotlinx.android.synthetic.main.rv_item_feed_profile.view.*
 
 class MatchFeedAdapter(imagesViewPool: RecyclerView.RecycledViewPool? = null) : LikeFeedAdapter(imagesViewPool) {
 
     override fun instantiateViewHolder(view: View): ProfileViewHolder =
         super.instantiateViewHolder(view).also { vh ->
             vh.profileImageAdapter.isLikeButtonVisible = false
+            (vh.itemView.ibtn_message.layoutParams as? ConstraintLayout.LayoutParams)
+                ?.apply { horizontalBias = 0.28f }?.let { vh.itemView.ibtn_message.layoutParams = it }
         }
 }
