5a8b784ceee149a20230953f2700794f2353c0a5
Maxim Alov <orcchg@yandex.ru>
2019-01-17 20:15:42 +0300

    Get rid of duplication of logic how viewState change is handle on Feed subscreens

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreFragment.kt
@@ -2,10 +2,7 @@ package com.ringoid.origin.feed.view.explore
 
 import android.os.Bundle
 import android.view.View
-import com.ringoid.base.view.ViewState
-import com.ringoid.origin.R
 import com.ringoid.origin.feed.view.FeedFragment
-import com.ringoid.origin.view.dialog.Dialogs
 import kotlinx.android.synthetic.main.fragment_feed.*
 
 class ExploreFragment : FeedFragment<ExploreViewModel>() {
@@ -15,24 +12,6 @@ class ExploreFragment : FeedFragment<ExploreViewModel>() {
     }
 
     override fun getVmClass(): Class<ExploreViewModel> = ExploreViewModel::class.java
-
-    // --------------------------------------------------------------------------------------------
-    override fun onViewStateChange(newState: ViewState) {
-        fun onIdleState() {
-            swipe_refresh_layout.isRefreshing = false
-        }
-
-        super.onViewStateChange(newState)
-        when (newState) {
-            is ViewState.IDLE -> onIdleState()
-            is ViewState.LOADING -> swipe_refresh_layout.isRefreshing = true
-            is ViewState.ERROR -> {
-                // TODO: analyze: newState.e
-                Dialogs.showTextDialog(activity, titleResId = R.string.error_common, description = "DL TEXT FROM URL")
-                onIdleState()
-            }
-        }
-    }
 
     /* Lifecycle */
     // --------------------------------------------------------------------------------------------
