b2beef92697e7830991eaa451645e98da1299d80
Maxim Alov <orcchg@yandex.ru>
2019-01-17 23:59:16 +0300

    Provide build script for windows as well

---
+++ assemble_win.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./assemble.sh /m/Android/sdk/build-tools/28.0.3
--- sign_apk.sh
+++ sign_apk.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
+function onMachine() {
+	unameOut="$(uname -s)"
+	case "${unameOut}" in
+		Linux*)     machine=Linux;;
+		Darwin*)    machine=Mac;;
+		CYGWIN*)    machine=Cygwin;;
+		MINGW*)     machine=MinGw;;
+		*)          machine="UNKNOWN:${unameOut}"
+	esac
+	echo ${machine}
+}
+
+function isOnWindows() {
+	if [ "$(onMachine)" = "MinGw" ]; then
+		echo "true"
+	else
+		echo "false"
+	fi
+}
+
 rm -rf ./release
 mkdir ./release
 STADPATH=${1:-~/Android/Sdk/build-tools/28.0.3}
-PASSWORD=${3:-}
+PASSWORD=${3:1q@w3e4r5t}
 echo "Standard path: ${STADPATH}"
 for FILE in `find ./app/build/outputs/apk/ -name "*.apk" -type f`
 do
@@ -15,12 +35,19 @@ do
 	echo "ASSEMBLE: ${FILE} ${filename} :: keystore=${keystorePath}"
 	filename="${filename%.*}"
 	${STADPATH}/zipalign -v -p 4 ${FILE} ./release/${filename}-aligned.apk
-	PASSWORD="q1W2e3r4t5"
+	APKSIGNER=${STADPATH}/apksigner
+	if [[ "$(isOnWindows)" == true ]]
+	then
+		APKSIGNER=${STADPATH}/apksigner.bat
+		echo "${APKSIGNER} on Windows"
+	else
+		echo "${APKSIGNER} on Unix"
+	fi
 	if [[ -z $PASSWORD ]]
 	then
-		${STADPATH}/apksigner sign --ks ${keystorePath} --out ./release/${filename}-aligned-signed.apk ./release/${filename}-aligned.apk
+		${APKSIGNER} sign --ks ${keystorePath} --out ./release/${filename}-aligned-signed.apk ./release/${filename}-aligned.apk
 	else
-		${STADPATH}/apksigner sign --ks ${keystorePath} --ks-pass pass:${PASSWORD} --out ./release/${filename}-aligned-signed.apk ./release/${filename}-aligned.apk
+		${APKSIGNER} sign --ks ${keystorePath} --ks-pass pass:${PASSWORD} --out ./release/${filename}-aligned-signed.apk ./release/${filename}-aligned.apk
 	fi
 	echo "ASSEMBLE: done ${filename}" ; ls -l ./release/${filename}-aligned-signed.apk
 done
