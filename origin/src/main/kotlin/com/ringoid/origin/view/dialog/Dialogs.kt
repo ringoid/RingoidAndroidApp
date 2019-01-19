78930f3a6eb3d01ceec13ec725360fc3e58929bd
Maxim Alov <orcchg@yandex.ru>
2019-01-19 12:14:51 +0300

    Maintain registry of dialogs, and ignore show dialog again if already shown, handle by hash code of input dialog's parameters

--- origin/src/main/kotlin/com/ringoid/origin/view/dialog/Dialogs.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/dialog/Dialogs.kt
@@ -10,73 +10,113 @@ import com.ringoid.origin.R
 
 object Dialogs {
 
+    private val registry = mutableSetOf<Long>()
+
+    private fun hashOf(activity: Activity?, @StringRes titleResId: Int, description: String?,
+                       @StringRes positiveBtnLabelResId: Int,
+                       @StringRes negativeBtnLabelResId: Int): Long {
+        var hash = 7L
+        hash = 31 * hash + (activity?.hashCode() ?: 0)
+        hash = 31 * hash + titleResId
+        hash = 31 * hash + (description?.hashCode() ?: 0)
+        hash = 31 * hash + positiveBtnLabelResId
+        hash = 31 * hash + negativeBtnLabelResId
+        return hash
+    }
+
+    private fun wrapListener(hash: Long, l: ((dialog: DialogInterface, which: Int) -> Unit)?)
+        : ((dialog: DialogInterface, which: Int) -> Unit)? =
+            { dialog, which ->
+                registry.remove(hash)
+                l?.invoke(dialog, which)
+                dialog.dismiss()
+            }
+
+    data class HashAlertDialog(val dialog: AlertDialog, val hash: Long)
+
     // --------------------------------------------------------------------------------------------
-    fun getTextDialog(activity: Activity?, @StringRes titleResId: Int = 0, @StringRes descriptionResId: Int,
-                      @StringRes positiveBtnLabelResId: Int = R.string.button_close,
-                      @StringRes negativeBtnLabelResId: Int = 0,
-                      positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
-                      negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null): AlertDialog =
+    private fun getTextDialog(
+            activity: Activity?, @StringRes titleResId: Int = 0, @StringRes descriptionResId: Int,
+            @StringRes positiveBtnLabelResId: Int = R.string.button_close,
+            @StringRes negativeBtnLabelResId: Int = 0,
+            positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+            negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null): HashAlertDialog =
         getTextDialog(activity, titleResId, activity?.resources?.getString(descriptionResId),
             positiveBtnLabelResId, negativeBtnLabelResId, positiveListener, negativeListener)
 
-    fun getTextDialog(activity: Activity?, @StringRes titleResId: Int = 0, description: String? = null,
-                      @StringRes positiveBtnLabelResId: Int = R.string.button_close,
-                      @StringRes negativeBtnLabelResId: Int = 0,
-                      positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
-                      negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null): AlertDialog =
+    private fun getTextDialog(
+            activity: Activity?, @StringRes titleResId: Int = 0, description: String? = null,
+            @StringRes positiveBtnLabelResId: Int = R.string.button_close,
+            @StringRes negativeBtnLabelResId: Int = 0,
+            positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+            negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null): HashAlertDialog =
         activity?.let {
+            val hash = hashOf(activity, titleResId, description, positiveBtnLabelResId, negativeBtnLabelResId)
             val builder = AlertDialog.Builder(activity)
             titleResId.takeIf { it != 0 }?.let { resId -> builder.also { it.setTitle(resId) } }
             description.takeIf { !it.isNullOrBlank() }?.let { str -> builder.also { it.setMessage(str) } }
-            positiveBtnLabelResId.takeIf { it != 0 }?.let { resId -> builder.also { it.setPositiveButton(resId, positiveListener) } }
-            negativeBtnLabelResId.takeIf { it != 0 }?.let { resId -> builder.also { it.setNegativeButton(resId, negativeListener) } }
-            builder.create()
+            positiveBtnLabelResId.takeIf { it != 0 }?.let { resId -> builder.also { it.setPositiveButton(resId, wrapListener(hash, positiveListener)) } }
+            negativeBtnLabelResId.takeIf { it != 0 }?.let { resId -> builder.also { it.setNegativeButton(resId, wrapListener(hash, negativeListener)) } }
+            HashAlertDialog(dialog = builder.create(), hash = hash)
         } ?: throw NullPointerException("Unable to show dialog: Activity is null")
 
-    fun showTextDialog(activity: BaseActivity<*>?, @StringRes titleResId: Int = 0, @StringRes descriptionResId: Int = 0,
-                       @StringRes positiveBtnLabelResId: Int = R.string.button_close,
-                       @StringRes negativeBtnLabelResId: Int = 0,
-                       positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
-                       negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) {
+    fun showTextDialog(
+            activity: BaseActivity<*>?, @StringRes titleResId: Int = 0, @StringRes descriptionResId: Int = 0,
+            @StringRes positiveBtnLabelResId: Int = R.string.button_close,
+            @StringRes negativeBtnLabelResId: Int = 0,
+            positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+            negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) {
         showTextDialog(activity, titleResId, descriptionResId.takeIf { it != 0}?.let { activity?.resources?.getString(it) },
             positiveBtnLabelResId, negativeBtnLabelResId, positiveListener, negativeListener)
     }
 
-    fun showTextDialog(activity: BaseActivity<*>?, @StringRes titleResId: Int = 0, description: String? = null,
-                       @StringRes positiveBtnLabelResId: Int = R.string.button_close,
-                       @StringRes negativeBtnLabelResId: Int = 0,
-                       positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
-                       negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) {
+    fun showTextDialog(
+            activity: BaseActivity<*>?, @StringRes titleResId: Int = 0, description: String? = null,
+            @StringRes positiveBtnLabelResId: Int = R.string.button_close,
+            @StringRes negativeBtnLabelResId: Int = 0,
+            positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+            negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) {
         activity?.takeIf { !it.isActivityDestroyed() }
                 ?.let {
-                    getTextDialog(it, titleResId, description,
+                    val dialog = getTextDialog(it, titleResId, description,
                         positiveBtnLabelResId, negativeBtnLabelResId,
-                        positiveListener, negativeListener).show()
+                        positiveListener, negativeListener)
+                    registry.takeIf { !it.contains(dialog.hash) }
+                                ?.add(dialog.hash)
+                                ?.also { dialog.dialog.show() }
                 }
     }
 
-    fun showTextDialog(activity: Activity?, @StringRes titleResId: Int = 0, @StringRes descriptionResId: Int = 0,
-                       @StringRes positiveBtnLabelResId: Int = R.string.button_close,
-                       @StringRes negativeBtnLabelResId: Int = 0,
-                       positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
-                       negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) =
+    fun showTextDialog(
+            activity: Activity?, @StringRes titleResId: Int = 0, @StringRes descriptionResId: Int = 0,
+            @StringRes positiveBtnLabelResId: Int = R.string.button_close,
+            @StringRes negativeBtnLabelResId: Int = 0,
+            positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+            negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) =
         showTextDialog(activity, titleResId, descriptionResId.takeIf { it != 0 }?.let { activity?.resources?.getString(it) },
             positiveBtnLabelResId, negativeBtnLabelResId, positiveListener, negativeListener)
 
-    fun showTextDialog(activity: Activity?, @StringRes titleResId: Int = 0, description: String? = null,
-                       @StringRes positiveBtnLabelResId: Int = R.string.button_close,
-                       @StringRes negativeBtnLabelResId: Int = 0,
-                       positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
-                       negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) =
+    fun showTextDialog(
+            activity: Activity?, @StringRes titleResId: Int = 0, description: String? = null,
+            @StringRes positiveBtnLabelResId: Int = R.string.button_close,
+            @StringRes negativeBtnLabelResId: Int = 0,
+            positiveListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+            negativeListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null) =
         activity?.takeIf { it is BaseActivity<*> }
                 ?.let {
                     showTextDialog(it as BaseActivity<*>, titleResId, description,
                         positiveBtnLabelResId, negativeBtnLabelResId,
                         positiveListener, negativeListener)
                 }
-                ?: getTextDialog(activity, titleResId, description,
-                    positiveBtnLabelResId, negativeBtnLabelResId,
-                    positiveListener, negativeListener).show()
+                ?: run {
+                    val dialog = getTextDialog(
+                        activity, titleResId, description,
+                        positiveBtnLabelResId, negativeBtnLabelResId,
+                        positiveListener, negativeListener)
+                    registry.takeIf { !it.contains(dialog.hash) }
+                                ?.add(dialog.hash)
+                                ?.also { dialog.dialog.show() }
+                }
 
     // --------------------------------------------------------------------------------------------
     fun getSingleChoiceDialog(activity: Activity?, items: Array<String>, l: ((dialog: DialogInterface, which: Int) -> Unit)? = null): AlertDialog? =
