e9aea61e48b9650e0d19088ccba9938c44e05532
Maxim Alov <orcchg@yandex.ru>
2019-01-21 14:21:35 +0300

    Refactoring FeedAdapter

---
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/BaseFeedAdapter.kt
@@ -0,0 +1,50 @@
+package com.ringoid.origin.feed.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jakewharton.rxbinding3.view.clicks
+import com.ringoid.base.adapter.BaseDiffCallback
+import com.ringoid.base.adapter.BaseListAdapter
+import com.ringoid.base.adapter.BaseViewHolder
+import com.ringoid.domain.model.feed.IProfile
+import com.ringoid.domain.model.feed.Profile
+import com.ringoid.origin.feed.model.ProfileImageVO
+import com.ringoid.origin.view.common.visibility_tracker.TrackingBus
+import com.ringoid.utility.clickDebounce
+import com.ringoid.utility.collection.EqualRange
+import kotlinx.android.synthetic.main.rv_item_feed_profile_content.view.*
+
+abstract class BaseFeedAdapter<T : IProfile, VH : BaseViewHolder<T>>(
+    private var imagesViewPool: RecyclerView.RecycledViewPool? = null, diffCb: BaseDiffCallback<T>)
+    : BaseListAdapter<T, VH>(diffCb) {
+
+    companion object {
+        const val VIEW_TYPE_NORMAL = 0
+        const val VIEW_TYPE_LOADING = 1
+        const val VIEW_TYPE_FEED_END = 2
+    }
+
+    var settingsClickListener: ((model: T, position: Int, positionOfImage: Int) -> Unit)? = null
+    internal var trackingBus: TrackingBus<EqualRange<ProfileImageVO>>? = null
+
+    init {
+        imagesViewPool = imagesViewPool ?: RecyclerView.RecycledViewPool()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH =
+        super.onCreateViewHolder(parent, viewType).apply {
+            trackingBus = this@BaseFeedAdapter.trackingBus
+            val wrapSettingsClickListener: ((model: T, position: Int) -> Unit)? =
+                { model: T, position: Int ->
+                    settingsClickListener?.invoke(model, position, getCurrentImagePosition())
+                }
+            itemView.ibtn_settings.clicks().compose(clickDebounce())
+                .subscribe { wrapOnItemClickListener(this, wrapSettingsClickListener).onClick(itemView.ibtn_settings) }
+            setOnClickListener(null)  // clicks on the whole feed's item is no-op
+        }
+
+    fun clear() {
+        submitList(emptyList())
+    }
+}
