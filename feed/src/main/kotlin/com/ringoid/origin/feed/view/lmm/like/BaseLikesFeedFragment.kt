ad42f1422cb2daf4549edc1675e4b5cfe0a3ae27
Maxim Alov <orcchg@yandex.ru>
2019-01-22 10:12:45 +0300

    Scroll item to top and open chat

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/lmm/like/BaseLikesFeedFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/lmm/like/BaseLikesFeedFragment.kt
@@ -1,10 +1,12 @@
 package com.ringoid.origin.feed.view.lmm.like
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ringoid.domain.model.feed.FeedItem
 import com.ringoid.origin.feed.adapter.lmm.LmmViewHolder
 import com.ringoid.origin.feed.adapter.lmm.like.BaseLikeFeedAdapter
 import com.ringoid.origin.feed.view.lmm.base.BaseLmmFeedFragment
 import com.ringoid.origin.feed.view.lmm.base.BaseLmmFeedViewModel
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 abstract class BaseLikesFeedFragment<VM : BaseLmmFeedViewModel, VH : LmmViewHolder>
     : BaseLmmFeedFragment<VM, VH>() {
@@ -13,8 +15,10 @@ abstract class BaseLikesFeedFragment<VM : BaseLmmFeedViewModel, VH : LmmViewHold
 
     override fun createFeedAdapter(): BaseLikeFeedAdapter<VH> =
         instantiateFeedAdapter().apply {
-            messageClickListener = { model: FeedItem, _ ->
-                // TODO: open chat
+            messageClickListener = { model: FeedItem, position: Int ->
+                (rv_items.layoutManager as LinearLayoutManager)
+                    .scrollToPositionWithOffset(position, 0)
+                // TOO: open chat
             }
         }
 }
