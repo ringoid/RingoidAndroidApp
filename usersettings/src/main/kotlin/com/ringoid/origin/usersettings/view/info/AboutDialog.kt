260b737f0e75aa6d330d41d159bd4fe2432dfb3a
Maxim Alov <orcchg@yandex.ru>
2019-01-23 23:33:24 +0300

    Make link on About dialog clickable and to be opened on Web Page screen and not to dismiss dialog earlier leaving it no change to detect coords clicked on link

--- usersettings/src/main/kotlin/com/ringoid/origin/usersettings/view/info/AboutDialog.kt
+++ usersettings/src/main/kotlin/com/ringoid/origin/usersettings/view/info/AboutDialog.kt
@@ -1,9 +1,12 @@
 package com.ringoid.origin.usersettings.view.info
 
 import android.os.Bundle
+import android.text.Spannable
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.jakewharton.rxbinding3.view.clicks
 import com.ringoid.base.view.SimpleBaseDialogFragment
 import com.ringoid.origin.navigation.navigate
@@ -33,6 +36,14 @@ class AboutDialog : SimpleBaseDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         btn_close.clicks().compose(clickDebounce()).subscribe { dismiss() }
         tv_description.movementMethod = object : AutoLinkMovementMethod() {
+            override fun onTouchEvent(widget: TextView, buffer: Spannable, event: MotionEvent): Boolean {
+                val result = super.onTouchEvent(widget, buffer, event)
+                if (event.action == MotionEvent.ACTION_UP) {
+                    widget.post { dismiss() }
+                }
+                return result
+            }
+
             override fun processUrl(url: String) {
                 navigate(this@AboutDialog, path = "/webpage?url=$url")
             }
