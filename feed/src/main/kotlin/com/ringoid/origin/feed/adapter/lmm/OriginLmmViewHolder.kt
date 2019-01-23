5b57bb2c261ce34df9c75a405408a23a9275bdb9
Maxim Alov <orcchg@yandex.ru>
2019-01-23 13:07:48 +0300

    Hide chat controll from feed items based on payload

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/OriginLmmViewHolder.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/OriginLmmViewHolder.kt
@@ -5,7 +5,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ringoid.base.adapter.BaseViewHolder
 import com.ringoid.domain.model.feed.FeedItem
 import com.ringoid.origin.feed.adapter.base.BaseFeedViewHolder
+import com.ringoid.origin.feed.adapter.base.FeedViewHolderHideControlls
+import com.ringoid.origin.feed.adapter.base.FeedViewHolderShowControlls
 import com.ringoid.origin.feed.adapter.base.OriginFeedViewHolder
+import com.ringoid.utility.changeVisibility
+import kotlinx.android.synthetic.main.rv_item_lmm_profile.view.*
 
 interface ILmmViewHolder
 
@@ -13,7 +17,18 @@ abstract class OriginLmmViewHolder(view: View, viewPool: RecyclerView.RecycledVi
     : OriginFeedViewHolder<FeedItem>(view, viewPool)
 
 abstract class BaseLmmViewHolder(view: View, viewPool: RecyclerView.RecycledViewPool? = null)
-    : BaseFeedViewHolder<FeedItem>(view, viewPool)
+    : BaseFeedViewHolder<FeedItem>(view, viewPool) {
+
+    override fun bind(model: FeedItem, payloads: List<Any>) {
+        if (payloads.contains(FeedViewHolderHideControlls)) {
+            itemView.ibtn_message.changeVisibility(isVisible = false)
+        }
+        if (payloads.contains(FeedViewHolderShowControlls)) {
+            itemView.ibtn_message.changeVisibility(isVisible = true)
+        }
+        super.bind(model, payloads)
+    }
+}
 
 open class HeaderLmmViewHolder(view: View) : BaseViewHolder<FeedItem>(view), ILmmViewHolder {
 
