0954059fab0020b0e3312f39ac82e709d7a17c44
Maxim Alov <orcchg@yandex.ru>
2019-01-24 17:56:49 +0300

    Ad basic implementation for Debug screen

--- usersettings/src/main/kotlin/com/ringoid/origin/usersettings/view/debug/DebugFragment.kt
+++ usersettings/src/main/kotlin/com/ringoid/origin/usersettings/view/debug/DebugFragment.kt
@@ -1,7 +1,12 @@
 package com.ringoid.origin.usersettings.view.debug
 
+import android.os.Bundle
+import android.view.View
+import com.jakewharton.rxbinding3.view.clicks
 import com.ringoid.base.view.BaseFragment
 import com.ringoid.usersettings.R
+import com.ringoid.utility.clickDebounce
+import kotlinx.android.synthetic.main.fragment_debug.*
 
 class DebugFragment : BaseFragment<DebugViewModel>() {
 
@@ -14,4 +19,16 @@ class DebugFragment : BaseFragment<DebugViewModel>() {
     override fun getVmClass(): Class<DebugViewModel> = DebugViewModel::class.java
 
     override fun getLayoutId(): Int = R.layout.fragment_debug
+
+    /* Lifecycle */
+    // --------------------------------------------------------------------------------------------
+    @Suppress("CheckResult", "AutoDispose")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        item_error_http.clicks().compose(clickDebounce()).subscribe {  }
+        item_error_token.clicks().compose(clickDebounce()).subscribe {  }
+        item_error_app_version.clicks().compose(clickDebounce()).subscribe {  }
+        item_error_server.clicks().compose(clickDebounce()).subscribe {  }
+        item_error_timeout.clicks().compose(clickDebounce()).subscribe {  }
+    }
 }
