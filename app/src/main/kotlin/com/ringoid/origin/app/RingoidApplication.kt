ab92b2cc8cab47197575b9494ebd3d47f4acbf44
Maxim Alov <orcchg@yandex.ru>
2019-01-29 12:31:28 +0300

    Temporary use develop enpoints' host

--- app/src/main/kotlin/com/ringoid/origin/app/RingoidApplication.kt
+++ app/src/main/kotlin/com/ringoid/origin/app/RingoidApplication.kt
@@ -2,6 +2,7 @@ package com.ringoid.origin.app
 
 import com.ringoid.data.remote.di.CloudModule
 import com.ringoid.data.remote.di.RingoidCloudModule
+import com.ringoid.data.remote.network.ApiUrlBase
 import com.ringoid.domain.BuildConfig
 import com.ringoid.origin.BaseRingoidApplication
 import com.ringoid.origin.app.di.DaggerApplicationComponent
@@ -15,6 +16,6 @@ class RingoidApplication : BaseRingoidApplication() {
             .application(this)
             .applicationContext(applicationContext)
             .cloudModule(CloudModule(appVersion = BuildConfig.BUILD_NUMBER))
-            .ringoidCloudModule(RingoidCloudModule())
+            .ringoidCloudModule(RingoidCloudModule(apiUrlBase = ApiUrlBase.DEVELOP))
             .create(this)
 }
