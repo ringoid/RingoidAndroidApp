27d4b6a55c31bb94f39c9c756ec6ee0683b51b6f
Maxim Alov <orcchg@yandex.ru>
2019-01-30 15:31:16 +0300

    Add trigger strategy for OPEN_CHAT aobj same as for VIEW aobj

--- domain/src/main/kotlin/com/ringoid/domain/model/actions/OpenChatActionObject.kt
+++ domain/src/main/kotlin/com/ringoid/domain/model/actions/OpenChatActionObject.kt
@@ -2,14 +2,19 @@ package com.ringoid.domain.model.actions
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import com.ringoid.domain.action_storage.DelayFromLast
+import com.ringoid.domain.action_storage.TriggerStrategy
+import com.ringoid.domain.action_storage.VIEW_DELAY_ON_TRIGGER
 
 class OpenChatActionObject(
     @Expose @SerializedName(COLUMN_OPEN_CHAT_COUNT) val count: Int = 1,
     @Expose @SerializedName(COLUMN_OPEN_CHAT_TIME_MILLIS) val timeInMillis: Long = 1,
     actionTime: Long = System.currentTimeMillis(),
-    sourceFeed: String, targetImageId: String, targetUserId: String)
+    sourceFeed: String, targetImageId: String, targetUserId: String,
+    triggerStrategies: List<TriggerStrategy> = listOf(DelayFromLast(VIEW_DELAY_ON_TRIGGER)))
     : ActionObject(actionTime = actionTime, actionType = "OPEN_CHAT", sourceFeed = sourceFeed,
-    targetImageId = targetImageId, targetUserId = targetUserId) {
+                   targetImageId = targetImageId, targetUserId = targetUserId,
+                   triggerStrategies = triggerStrategies) {
 
     companion object {
         const val COLUMN_OPEN_CHAT_COUNT = "openChatCount"
