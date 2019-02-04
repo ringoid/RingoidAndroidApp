84f9e0b9c13425447a7fe3a90f7a0577f6230b53
Maxim Alov <orcchg@yandex.ru>
2019-02-04 10:08:55 +0300

    Don't purge LMM feeds on Lmm tab switch

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/lmm/like/BaseLikesFeedFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/lmm/like/BaseLikesFeedFragment.kt
@@ -6,15 +6,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.ringoid.base.observe
-import com.ringoid.base.view.ViewState
 import com.ringoid.domain.model.feed.FeedItem
 import com.ringoid.origin.feed.adapter.base.OriginFeedViewHolder
 import com.ringoid.origin.feed.adapter.lmm.like.BaseLikeFeedAdapter
 import com.ringoid.origin.feed.view.lmm.ILmmFragment
 import com.ringoid.origin.feed.view.lmm.base.BaseLmmFeedFragment
 import com.ringoid.origin.feed.view.lmm.base.BaseLmmFeedViewModel
-import com.ringoid.origin.messenger.view.ChatFragment
 import com.ringoid.origin.messenger.ChatPayload
+import com.ringoid.origin.messenger.view.ChatFragment
 import com.ringoid.utility.communicator
 
 abstract class BaseLikesFeedFragment<VM : BaseLmmFeedViewModel, VH : OriginFeedViewHolder<FeedItem>>
@@ -53,16 +52,5 @@ abstract class BaseLikesFeedFragment<VM : BaseLmmFeedViewModel, VH : OriginFeedV
          */
         communicator(ILmmFragment::class.java)?.accessViewModel()
             ?.let { parentVm -> viewLifecycleOwner.observe(parentVm.listScrolls, ::scrollListToPosition) }
-    }
-
-    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
-        super.setUserVisibleHint(isVisibleToUser)
-        if (isActivityCreated && !isVisibleToUser) {
-            /**
-             * Purge feed when Main tab has switched back to Explore screen,
-             * swipe-to-refresh is required to get need data.
-             */
-            vm.clearScreen(mode = ViewState.CLEAR.MODE_NEED_REFRESH)
-        }
     }
 }
