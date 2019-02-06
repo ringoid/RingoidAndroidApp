3aaff34e50c7e3d9b09f0615045f2bdccb3e4f4f
Maxim Alov <orcchg@yandex.ru>
2019-02-06 14:12:51 +0300

    Fix empty collection cannot be reduced

--- domain/src/main/kotlin/com/ringoid/domain/model/feed/Lmm.kt
+++ domain/src/main/kotlin/com/ringoid/domain/model/feed/Lmm.kt
@@ -6,7 +6,7 @@ data class Lmm(val likes: List<FeedItem>, val matches: List<FeedItem>, val messa
     fun isMatchesEmpty(): Boolean = matches.isEmpty()
     fun isMessagesEmpty(): Boolean = messages.isEmpty()
 
-    fun messagesCount(): Int = messages.map { it.messages.size }.reduce { acc, i -> acc + i }
+    fun messagesCount(): Int = if (messages.isEmpty()) 0 else messages.map { it.messages.size }.reduce { acc, i -> acc + i }
     fun newLikesCount(): Int = likes.count { it.isNotSeen }
     fun newMatchesCount(): Int = matches.count { it.isNotSeen }
     fun containsNotSeenItems(): Boolean = newLikesCount() > 0 || newMatchesCount() > 0
