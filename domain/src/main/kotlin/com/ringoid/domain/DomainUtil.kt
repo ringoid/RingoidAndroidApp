e581d197a3026646dfad67dc8de3446dd5f18e29
Maxim Alov <orcchg@yandex.ru>
2019-02-06 09:41:00 +0300

    Start load more is at least 5 items left to end

--- domain/src/main/kotlin/com/ringoid/domain/DomainUtil.kt
+++ domain/src/main/kotlin/com/ringoid/domain/DomainUtil.kt
@@ -8,7 +8,7 @@ object DomainUtil {
     const val CURRENT_USER_ID: String = "currentUserId"
     const val DEBOUNCE_NET = 400L
     const val LIMIT_PER_PAGE = 20
-    const val LOAD_MORE_THRESHOLD = 1  // items left to end
+    const val LOAD_MORE_THRESHOLD = 5  // items left to end
 
     const val CLIPBOARD_KEY_CHAT_MESSAGE = "chatMessage"
     const val CLIPBOARD_KEY_CUSTOMER_ID = "customerId"
