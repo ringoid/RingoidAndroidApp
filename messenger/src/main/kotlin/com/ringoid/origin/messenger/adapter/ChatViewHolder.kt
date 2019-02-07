7bb6b56878517d71bfb56c22f7e26b59d3951c85
Maxim Alov <orcchg@yandex.ru>
2019-02-07 15:04:58 +0300

    Chat viewHolder removed excess classes

--- messenger/src/main/kotlin/com/ringoid/origin/messenger/adapter/ChatViewHolder.kt
+++ messenger/src/main/kotlin/com/ringoid/origin/messenger/adapter/ChatViewHolder.kt
@@ -5,18 +5,16 @@ import com.ringoid.base.adapter.BaseViewHolder
 import com.ringoid.domain.model.messenger.Message
 import kotlinx.android.synthetic.main.rv_item_chat_item.view.*
 
-abstract class BaseChatViewHolder(view: View) : BaseViewHolder<Message>(view)
-
-open class ChatViewHolder(view: View) : BaseChatViewHolder(view) {
+abstract class BaseChatViewHolder(view: View) : BaseViewHolder<Message>(view) {
 
     override fun bind(model: Message) {
         itemView.tv_chat_message.text = model.text
     }
 }
 
-class MyChatViewHolder(view: View) : ChatViewHolder(view)
+class MyChatViewHolder(view: View) : BaseChatViewHolder(view)
 
-class PeerChatViewHolder(view: View) : ChatViewHolder(view)
+class PeerChatViewHolder(view: View) : BaseChatViewHolder(view)
 
 class HeaderChatViewHolder(view: View) : BaseChatViewHolder(view) {
 
