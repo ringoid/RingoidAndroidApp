1ccae8e8809ab90febe2e2f8c2730559c6a38b48
Maxim Alov <orcchg@yandex.ru>
2019-02-07 19:35:11 +0300

    Disabled fling gesture

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/LmmViewHolder.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/LmmViewHolder.kt
@@ -8,8 +8,6 @@ import com.ringoid.origin.feed.adapter.base.FeedViewHolderHideControls
 import com.ringoid.origin.feed.adapter.base.FeedViewHolderShowControls
 import com.ringoid.origin.feed.adapter.base.OriginFeedViewHolder
 import com.ringoid.utility.changeVisibility
-import com.ringoid.widget.view.Direction
-import kotlinx.android.synthetic.main.rv_item_feed_profile_content.view.*
 import kotlinx.android.synthetic.main.rv_item_lmm_profile.view.*
 
 open class LmmViewHolder(view: View, viewPool: RecyclerView.RecycledViewPool? = null)
@@ -17,17 +15,17 @@ open class LmmViewHolder(view: View, viewPool: RecyclerView.RecycledViewPool? = 
 
     init {
         // TODO: improve UX for fling
-        itemView.ibtn_message.setOnFlingListener {
-            val rv = itemView.rv_items
-            when (it) {
-                Direction.left -> {  // next page
-                    rv.smoothScrollToPosition(adapterPosition + 1)
-                }
-                Direction.right -> {  // previous page
-                    rv.smoothScrollToPosition(adapterPosition - 1)
-                }
-            }
-        }
+//        itemView.ibtn_message.setOnFlingListener {
+//            val rv = itemView.rv_items
+//            when (it) {
+//                Direction.left -> {  // next page
+//                    rv.smoothScrollToPosition(adapterPosition + 1)
+//                }
+//                Direction.right -> {  // previous page
+//                    rv.smoothScrollToPosition(adapterPosition - 1)
+//                }
+//            }
+//        }
     }
 
     override fun bind(model: FeedItem, payloads: List<Any>) {
