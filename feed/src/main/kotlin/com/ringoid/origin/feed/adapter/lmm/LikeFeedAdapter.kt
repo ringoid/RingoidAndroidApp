40fd4bfd2a24be991bce1c35b4ee62ca9569dc17
Maxim Alov <orcchg@yandex.ru>
2019-02-07 15:41:56 +0300

    Use click on image from LikesYou to fire LIKE/UNLIKE event

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/LikeFeedAdapter.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/LikeFeedAdapter.kt
@@ -1,4 +1,23 @@
 package com.ringoid.origin.feed.adapter.lmm
 
-class LikeFeedAdapter : BaseLmmAdapter()
+import android.view.ViewGroup
+import com.ringoid.domain.model.feed.FeedItem
+import com.ringoid.origin.feed.adapter.base.BaseFeedViewHolder
+import com.ringoid.origin.feed.adapter.base.OriginFeedViewHolder
+import com.ringoid.origin.feed.model.ProfileImageVO
+
+class LikeFeedAdapter : BaseLmmAdapter() {
+
+    var onLikeImageListener: ((model: ProfileImageVO, position: Int) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OriginFeedViewHolder<FeedItem> {
+        val viewHolder = super.onCreateViewHolder(parent, viewType)
+        return viewHolder  // perform additional initialization only for VIEW_TYPE_NORMAL view holders
+            .takeIf { viewType == VIEW_TYPE_NORMAL }
+            ?.let { it as? BaseFeedViewHolder<FeedItem> }
+            ?.also { vh ->
+                vh.profileImageAdapter.itemClickListener = onLikeImageListener
+            } ?: viewHolder  // don't apply additional initializations on non-VIEW_TYPE_NORMAL view holders
+    }
+}
 // TODO: show message button only after like tap
