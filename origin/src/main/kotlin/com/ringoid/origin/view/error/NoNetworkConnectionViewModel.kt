225549d7f87a54f0862610ce22b9426a51d28e76
Maxim Alov <orcchg@yandex.ru>
2019-02-11 17:51:55 +0300

    Delay before retry on NoConnection screen

--- origin/src/main/kotlin/com/ringoid/origin/view/error/NoNetworkConnectionViewModel.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/error/NoNetworkConnectionViewModel.kt
@@ -3,12 +3,15 @@ package com.ringoid.origin.view.error
 import android.app.Application
 import com.ringoid.base.view.ViewState
 import com.ringoid.base.viewmodel.BaseViewModel
+import com.ringoid.utility.delay
 import javax.inject.Inject
 
 class NoNetworkConnectionViewModel @Inject constructor(app: Application) : BaseViewModel(app) {
 
     fun onPageReload() {
-        if (connectionManager.isNetworkAvailable()) viewState.value = ViewState.CLOSE
-        else viewState.value = ViewState.IDLE
+        delay(delay = 1000L) {
+            if (connectionManager.isNetworkAvailable()) viewState.value = ViewState.CLOSE
+            else viewState.value = ViewState.IDLE
+        }
     }
 }
