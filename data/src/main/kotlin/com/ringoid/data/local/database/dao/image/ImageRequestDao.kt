6d6604880b00b20df29e4289daa83df8e8fad12d
Maxim Alov <orcchg@yandex.ru>
2019-02-13 09:54:20 +0300

    Get saved image requests from db

--- data/src/main/kotlin/com/ringoid/data/local/database/dao/image/ImageRequestDao.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/dao/image/ImageRequestDao.kt
@@ -3,10 +3,15 @@ package com.ringoid.data.local.database.dao.image
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.ringoid.data.local.database.model.image.ImageRequestDbo
+import io.reactivex.Single
 
 @Dao
 interface ImageRequestDao {
+
+    @Query("SELECT * FROM ${ImageRequestDbo.TABLE_NAME}")
+    fun requests(): Single<List<ImageRequestDbo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addRequest(request: ImageRequestDbo)
