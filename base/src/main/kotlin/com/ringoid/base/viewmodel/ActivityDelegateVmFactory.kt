878a7ce6a7b772d91824c650625ec84e724d9ae7
Maxim Alov macbook <orcchg@gmail.com>
2019-02-16 12:05:49 +0300

    Removed excess class

--- base/src/main/kotlin/com/ringoid/base/viewmodel/ActivityDelegateVmFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.ringoid.base.viewmodel
-
-class ActivityDelegateVmFactory {
-
-//    operator fun getValue(thisRef: Any, property: KProperty<*>): ViewModelProvider.Factory {
-//        // TODO: use dagger binds
-//        return DaggerViewModelFactory<>()
-//    }
-}
--- base/src/main/kotlin/com/ringoid/base/viewmodel/FragmentDelegateVmFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.ringoid.base.viewmodel
-
-class FragmentDelegateVmFactory {
-
-//    operator fun getValue(thisRef: Any, property: KProperty<*>): ViewModelProvider.Factory {
-//        // TODO: use dagger binds
-//        return DaggerViewModelFactory<>()
-//    }
-}
