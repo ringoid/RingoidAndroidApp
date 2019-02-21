a4eaf1091e8e96a28639e3657fe6b3d7a1d3ba1f
Maxim Alov <orcchg@yandex.ru>
2019-02-21 19:56:11 +0300

    Set Splash theme on Splash screen for sure

--- origin/src/main/kotlin/com/ringoid/origin/view/splash/SplashActivity.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/splash/SplashActivity.kt
@@ -3,12 +3,17 @@ package com.ringoid.origin.view.splash
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.ringoid.base.view.SimpleBaseActivity
+import com.ringoid.origin.R
 import com.ringoid.origin.navigation.splash
 
 class SplashActivity : SimpleBaseActivity() {
 
     /* Lifecycle */
     // --------------------------------------------------------------------------------------------
+    override fun onBeforeCreate() {
+        setTheme(R.style.SplashTheme)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
