bb6aa21e80ac9dbd94179d0801db6a993758ad62
Maxim Alov <orcchg@yandex.ru>
2019-03-06 13:32:32 +0300

    Log Sentry warnings to DebugLog as well

--- domain/src/main/kotlin/com/ringoid/domain/debug/DebugLogUtil.kt
+++ domain/src/main/kotlin/com/ringoid/domain/debug/DebugLogUtil.kt
@@ -2,6 +2,7 @@ package com.ringoid.domain.debug
 
 import com.ringoid.domain.BuildConfig
 import io.reactivex.subjects.PublishSubject
+import io.sentry.event.Event
 
 @DebugOnly
 object DebugLogUtil {
@@ -14,6 +15,16 @@ object DebugLogUtil {
     fun w(log: String) = log(log, DebugLogLevel.WARNING)
     fun e(log: String) = log(log, DebugLogLevel.ERROR)
     fun e(e: Throwable) = log(log = "${e.javaClass.simpleName}: ${e.message}".trim(), level = DebugLogLevel.ERROR)
+
+    fun log(log: String, level: Event.Level) {
+        when (level) {
+            Event.Level.DEBUG -> d(log)
+            Event.Level.INFO -> i(log)
+            Event.Level.WARNING -> w(log)
+            Event.Level.ERROR -> e(log)
+            Event.Level.FATAL -> e(log)
+        }
+    }
 
     private fun log(log: String, level: DebugLogLevel = DebugLogLevel.DEBUG) {
         if (BuildConfig.DEBUG) {
