3a626fe83d01eb52aeebcddbff790dcff3eaf3fa
Maxim Alov <orcchg@yandex.ru>
2019-03-06 08:52:56 +0300

    Remove debug logs

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/base/FeedItemAnimator.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/base/FeedItemAnimator.kt
@@ -2,7 +2,6 @@ package com.ringoid.origin.feed.adapter.base
 
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.RecyclerView
-import timber.log.Timber
 
 class FeedItemAnimator(l: ((position: Int) -> Unit)? = null) : DefaultItemAnimator() {
 
@@ -13,7 +12,6 @@ class FeedItemAnimator(l: ((position: Int) -> Unit)? = null) : DefaultItemAnimat
     override fun onRemoveFinished(item: RecyclerView.ViewHolder) {
         super.onRemoveFinished(item)
         removed = true
-        Timber.w("ANIM: ${item.adapterPosition}")
     }
 
     override fun onMoveFinished(item: RecyclerView.ViewHolder) {
@@ -21,7 +19,6 @@ class FeedItemAnimator(l: ((position: Int) -> Unit)? = null) : DefaultItemAnimat
         if (removed) {
             removed = false
             onItemRemoved?.invoke(item.adapterPosition)
-            Timber.e("ANIMA: ${item.adapterPosition}")
         }
     }
 }
