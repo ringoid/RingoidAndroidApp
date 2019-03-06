adb733ad7885a099b50808b99b10ae22b8cab875
Maxim Alov <orcchg@yandex.ru>
2019-03-06 18:50:11 +0300

    Fix typo in logs

--- utility/src/main/kotlin/com/ringoid/utility/DateTimeUtils.kt
+++ utility/src/main/kotlin/com/ringoid/utility/DateTimeUtils.kt
@@ -5,7 +5,7 @@ import java.util.*
 fun isAdultAge(yearOfBirth: Int, calendar: Calendar, adultThreshold: Int = 18): Boolean =
     yearOfBirth in 1938..(calendar.get(Calendar.YEAR) - adultThreshold)
 
-fun Date.time(): String = "${wrapTimeUnit(hours)}:${wrapTimeUnit(minutes)}:${wrapTimeUnit(seconds)}}"
+fun Date.time(): String = "${wrapTimeUnit(hours)}:${wrapTimeUnit(minutes)}:${wrapTimeUnit(seconds)}"
 
 fun wrapTimeUnit(unit: Int): String = if (unit < 10) "0$unit" else "$unit"
 fun wrapMillisUnit(unit: Long): String = if (unit < 10) "00$unit" else if (unit < 100) "0$unit" else "$unit"
