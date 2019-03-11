8b3b4e7b510d94fa246c3bed9fca2ce18602940e
Maxim Alov macbook <orcchg@gmail.com>
2019-03-11 16:50:32 +0300

    Show image id on profile photos

--- profile/src/main/kotlin/com/ringoid/origin/profile/adapter/UserProfileImageViewHolder.kt
+++ profile/src/main/kotlin/com/ringoid/origin/profile/adapter/UserProfileImageViewHolder.kt
@@ -3,7 +3,9 @@ package com.ringoid.origin.profile.adapter
 import android.view.View
 import com.bumptech.glide.request.RequestOptions
 import com.ringoid.base.adapter.BaseViewHolder
+import com.ringoid.domain.BuildConfig
 import com.ringoid.domain.model.image.UserImage
+import com.ringoid.utility.changeVisibility
 import com.ringoid.utility.image.ImageLoader
 import kotlinx.android.synthetic.main.rv_item_user_profile_image.view.*
 
@@ -12,12 +14,20 @@ abstract class BaseUserProfileImageViewHolder(view: View) : BaseViewHolder<UserI
 
 class UserProfileImageViewHolder(view: View) : BaseUserProfileImageViewHolder(view) {
 
+    init {
+        itemView.tv_image_id.changeVisibility(isVisible = BuildConfig.IS_STAGING)
+    }
+
     override fun bind(model: UserImage) {
         ImageLoader.load(uri = model.uri, thumbnailUri = model.uriLocal,
             imageView = itemView.iv_image,
             options = RequestOptions().centerCrop())
 
         itemView.tv_likes_count.text = "${model.numberOfLikes}"
+
+        if (BuildConfig.IS_STAGING) {
+            itemView.tv_image_id.text = model.hashIdWithResolution()
+        }
     }
 }
 
