9402a9d8d88acd21654d9b2638f47f4d1dea71ee
Maxim Alov macbook <orcchg@gmail.com>
2019-03-11 18:42:21 +0300

    Fix resizing of debug view

--- origin/src/main/kotlin/com/ringoid/origin/view/debug/DebugView.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/debug/DebugView.kt
@@ -1,11 +1,12 @@
 package com.ringoid.origin.view.debug
 
-import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jakewharton.rxbinding3.view.clicks
@@ -15,7 +16,10 @@ import com.ringoid.domain.debug.DebugOnly
 import com.ringoid.domain.debug.EmptyDebugLogItem
 import com.ringoid.origin.R
 import com.ringoid.origin.WidgetR_drawable
-import com.ringoid.utility.*
+import com.ringoid.utility.clickDebounce
+import com.ringoid.utility.copyToClipboard
+import com.ringoid.utility.linearLayoutManager
+import com.ringoid.utility.toast
 import com.uber.autodispose.AutoDispose.autoDisposable
 import com.uber.autodispose.android.scope
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -30,6 +34,8 @@ class DebugView : ConstraintLayout {
 
     companion object {
         private var MIN_HEIGHT = -1
+        private var MAX_LP: ViewGroup.LayoutParams? = null
+        private var MIN_LP: ViewGroup.LayoutParams? = null
     }
 
     constructor(context: Context): this(context, null)
@@ -45,6 +51,9 @@ class DebugView : ConstraintLayout {
     @Suppress("CheckResult")
     private fun init(context: Context) {
         if (MIN_HEIGHT == -1) MIN_HEIGHT = resources.getDimensionPixelSize(R.dimen.widget_debug_height)
+        if (MAX_LP == null) MAX_LP = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
+        if (MIN_LP == null) MIN_LP = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, MIN_HEIGHT)
+            .apply { bottomToBottom = ConstraintSet.PARENT_ID }
 
         background = ContextCompat.getDrawable(context, WidgetR_drawable.rect_white_80_opaque)
         LayoutInflater.from(context).inflate(getLayoutId(), this, true)
@@ -62,7 +71,6 @@ class DebugView : ConstraintLayout {
             context.copyToClipboard(key = DomainUtil.CLIPBOARD_KEY_DEBUG, value = debugLogItemAdapter.getContentText())
             context.toast(R.string.common_clipboard)
         }
-        minimize()
     }
 
     override fun onAttachedToWindow() {
@@ -85,11 +93,11 @@ class DebugView : ConstraintLayout {
 
     private fun maximize() {
         sizeToggle = true
-        minimumHeight = (context as? Activity)?.getScreenHeight() ?: MIN_HEIGHT * 3
+        layoutParams = MAX_LP
     }
 
     private fun minimize() {
         sizeToggle = false
-        minimumHeight = MIN_HEIGHT
+        layoutParams = MIN_LP
     }
 }
