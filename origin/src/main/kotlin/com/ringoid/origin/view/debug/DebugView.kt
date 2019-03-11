13592fde4fd948856db0734cae548de0192bd2fe
Maxim Alov macbook <orcchg@gmail.com>
2019-03-11 18:17:43 +0300

    Add maximize icon for debug view

--- origin/src/main/kotlin/com/ringoid/origin/view/debug/DebugView.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/debug/DebugView.kt
@@ -1,7 +1,7 @@
 package com.ringoid.origin.view.debug
 
+import android.app.Activity
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.annotation.LayoutRes
@@ -13,12 +13,9 @@ import com.ringoid.domain.DomainUtil
 import com.ringoid.domain.debug.DebugLogUtil
 import com.ringoid.domain.debug.DebugOnly
 import com.ringoid.domain.debug.EmptyDebugLogItem
-import com.ringoid.utility.clickDebounce
-import com.ringoid.utility.copyToClipboard
-import com.ringoid.utility.toast
 import com.ringoid.origin.R
 import com.ringoid.origin.WidgetR_drawable
-import com.ringoid.utility.linearLayoutManager
+import com.ringoid.utility.*
 import com.uber.autodispose.AutoDispose.autoDisposable
 import com.uber.autodispose.android.scope
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -29,6 +26,11 @@ import timber.log.Timber
 class DebugView : ConstraintLayout {
 
     private val debugLogItemAdapter = DebugLogItemAdapter { postDelayed({ rv_debug_items.linearLayoutManager()?.scrollToPositionWithOffset(it - 1, 0) }, 100L) }
+    private var sizeToggle = false
+
+    companion object {
+        private var MIN_HEIGHT = -1
+    }
 
     constructor(context: Context): this(context, null)
 
@@ -42,8 +44,9 @@ class DebugView : ConstraintLayout {
 
     @Suppress("CheckResult")
     private fun init(context: Context) {
+        if (MIN_HEIGHT == -1) MIN_HEIGHT = resources.getDimensionPixelSize(R.dimen.widget_debug_height)
+
         background = ContextCompat.getDrawable(context, WidgetR_drawable.rect_white_80_opaque)
-        minimumHeight = resources.getDimensionPixelSize(R.dimen.widget_debug_height)
         LayoutInflater.from(context).inflate(getLayoutId(), this, true)
 
         rv_debug_items.apply {
@@ -52,10 +55,14 @@ class DebugView : ConstraintLayout {
         }
 
         ibtn_clear_debug.clicks().compose(clickDebounce()).subscribe { clear() }
+        ibtn_resize_debug.clicks().compose(clickDebounce()).subscribe {
+            if (sizeToggle) minimize() else maximize()
+        }
         ibtn_share_debug.clicks().compose(clickDebounce()).subscribe {
             context.copyToClipboard(key = DomainUtil.CLIPBOARD_KEY_DEBUG, value = debugLogItemAdapter.getContentText())
             context.toast(R.string.common_clipboard)
         }
+        minimize()
     }
 
     override fun onAttachedToWindow() {
@@ -74,5 +81,15 @@ class DebugView : ConstraintLayout {
 
     private fun clear() {
         debugLogItemAdapter.clear()
+    }
+
+    private fun maximize() {
+        sizeToggle = true
+        minimumHeight = (context as? Activity)?.getScreenHeight() ?: MIN_HEIGHT * 3
+    }
+
+    private fun minimize() {
+        sizeToggle = false
+        minimumHeight = MIN_HEIGHT
     }
 }
