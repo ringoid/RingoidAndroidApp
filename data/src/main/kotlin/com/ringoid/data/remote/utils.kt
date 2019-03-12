50aa0c6aac3d268332c365d1bd7b4c27031fdec4
Maxim Alov macbook <orcchg@gmail.com>
2019-03-12 17:48:18 +0300

    Add 70 ms which is approximate overhead between doOnNext() and doFinally() calls

--- data/src/main/kotlin/com/ringoid/data/remote/utils.kt
+++ data/src/main/kotlin/com/ringoid/data/remote/utils.kt
@@ -99,7 +99,7 @@ inline fun <reified T : BaseResponse> logResponseObservable(tag: String = DEFAUL
 // ----------------------------------------------
 fun checkElapsedTimeAndWarn(startTime: Long, tag: String = DEFAULT_TAG) {
     val elapsedTime = System.currentTimeMillis() - startTime
-    if (elapsedTime >= BuildConfig.RESPONSE_TIME_THRESHOLD) {
+    if (elapsedTime >= BuildConfig.RESPONSE_TIME_THRESHOLD + 70) {
         val message = "Waiting for $tag longer than expected ${BuildConfig.RESPONSE_TIME_THRESHOLD} ms"; Timber.w(message)
         SentryUtil.w(message, listOf("elapsed time" to "$elapsedTime", "threshold" to "${BuildConfig.RESPONSE_TIME_THRESHOLD}"))
         DebugLogUtil.w("$message [$tag], duration=$elapsedTime")
