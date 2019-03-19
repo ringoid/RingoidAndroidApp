92922d8aabbd42ab07864ae9f70ef2640883db0a
Maxim Alov macbook <orcchg@gmail.com>
2019-03-19 16:59:33 +0300

    Crash fix

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/base/BaseFeedAdapter.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/base/BaseFeedAdapter.kt
@@ -27,7 +27,11 @@ abstract class BaseFeedAdapter(diffCb: BaseDiffCallback<FeedItemVO>, headerRows:
         return viewHolder  // perform additional initialization only for VIEW_TYPE_NORMAL view holders
             .takeIf { viewType == VIEW_TYPE_NORMAL }
             ?.also { vh ->
-                vh.snapPositionListener = { getModel(vh.adapterPosition).positionOfImage = it }
+                vh.snapPositionListener = { positionOfImage ->
+                    vh.adapterPosition
+                        .takeIf { it != RecyclerView.NO_POSITION }
+                        ?.let { getModel(it).positionOfImage = positionOfImage }
+                }
                 vh.trackingBus = this@BaseFeedAdapter.trackingBus
                 val wrapSettingsClickListener: ((model: FeedItemVO, position: Int) -> Unit)? =
                     { model: FeedItemVO, position: Int -> settingsClickListener?.invoke(model, position, vh.getCurrentImagePosition()) }
