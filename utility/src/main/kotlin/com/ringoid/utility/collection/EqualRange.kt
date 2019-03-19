e4d081fc91cce2b20503f6d77123160a0064c50e
Maxim Alov macbook <orcchg@gmail.com>
2019-03-19 10:29:47 +0300

    Introduced orientation for EqualRange

--- utility/src/main/kotlin/com/ringoid/utility/collection/EqualRange.kt
+++ utility/src/main/kotlin/com/ringoid/utility/collection/EqualRange.kt
@@ -1,9 +1,12 @@
 package com.ringoid.utility.collection
 
-class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(items) {
+class EqualRange<T>(val from: Int, val to: Int, val orientation: Orientation, items: List<T>) : ArrayList<T>(items) {
+
+    enum class Orientation { HORIZONTAL, VERTICAL, NONE }
 
     companion object {
-        fun <T> empty(): EqualRange<T> = EqualRange(-1, -1, emptyList())
+        fun <T> empty(orientation: Orientation = Orientation.NONE): EqualRange<T> =
+            EqualRange(-1, -1, orientation, emptyList())
     }
 
     init {
@@ -32,6 +35,9 @@ class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(i
      *  [c  [a   b]  d] -> []
      */
     fun delta(new: EqualRange<T>): EqualRange<T> {
+        if (orientation != new.orientation) {
+            throw IllegalArgumentException("Orientations are different while calculating delta")
+        }
         if (isRangeEmpty()) {
             return this
         }
@@ -39,47 +45,47 @@ class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(i
          * [a  [c  b]  d] => [a  c)
          */
         if (new.from in from..to && to in new.from..new.to) {
-            if (from < new.from) return EqualRange(from, new.from - 1, subList(0, new.from - from))
-            if (from == new.from) return EqualRange.empty()
+            if (from < new.from) return EqualRange(from, new.from - 1, orientation, subList(0, new.from - from))
+            if (from == new.from) return EqualRange.empty(orientation)
         }
         /**
          * [a|c  d]  b] => (d  b]
          */
         if (from == new.from && new.to < to) {
-            return EqualRange(new.to + 1, to, subList(new.to + 1 - from, to - from + 1))
+            return EqualRange(new.to + 1, to, orientation, subList(new.to + 1 - from, to - from + 1))
         }
         /**
          * [a  [c  d|b] => [a  c)
          */
         if (from < new.from && to == new.to) {
-            return EqualRange(from, new.to - 1, subList(0, new.to - from))
+            return EqualRange(from, new.to - 1, orientation, subList(0, new.to - from))
         }
         /**
          * [a  [c   d]  b] => [a  c)(d  b]
          */
         if (new.from in from..to && to > new.to) {
             val l = subList(0, new.from + 1 - from).apply { addAll(subList(new.to + 1 - from, to + 1 - from)) }
-            return EqualRange(from, from + l.size + 1, l)
+            return EqualRange(from, from + l.size + 1, orientation, l)
         }
         /**
          * [c  [a  d]  b] => (d  b]
          */
         if (from in new.from..new.to && new.to in from..to) {
-            if (new.to < to) return EqualRange(new.to + 1, to, subList(new.to + 1 - from, to + 1 - from))
-            if (new.to == to) return EqualRange.empty()
+            if (new.to < to) return EqualRange(new.to + 1, to, orientation, subList(new.to + 1 - from, to + 1 - from))
+            if (new.to == to) return EqualRange.empty(orientation)
         }
         /**
          * [a  b][c  d], [c  d][a  b] => [a  b]
          */
-        if (to < new.from || from > new.to) return EqualRange(from, to, this)
+        if (to < new.from || from > new.to) return EqualRange(from, to, orientation, this)
         /**
          * [a  b|c  d] => [a  b)
          */
-        if (to <= new.from) return EqualRange(from, to - 1, dropLast(1))
+        if (to <= new.from) return EqualRange(from, to - 1, orientation, dropLast(1))
         /**
          * [c  d|a  b] => (a  b]
          */
-        if (from >= new.to) return EqualRange(from + 1, to, drop(1))
+        if (from >= new.to) return EqualRange(from + 1, to, orientation, drop(1))
 
         return this
     }
@@ -87,15 +93,15 @@ class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(i
     fun dropItems(n: Int): EqualRange<T> {
         if (n < 0) throw IllegalArgumentException("Number of first items to drop must not be negative: $n")
         if (n == 0) return this
-        if (n >= size) return empty()
-        return EqualRange(from + n, to, drop(n))
+        if (n >= size) return empty(orientation)
+        return EqualRange(from + n, to, orientation, drop(n))
     }
 
     fun dropLastItems(n: Int): EqualRange<T> {
         if (n < 0) throw IllegalArgumentException("Number of first items to drop must not be negative: $n")
         if (n == 0) return this
-        if (n >= size) return empty()
-        return EqualRange(from, to - n, dropLast(n))
+        if (n >= size) return empty(orientation)
+        return EqualRange(from, to - n, orientation, dropLast(n))
     }
 
     // ------------------------------------------
@@ -109,6 +115,7 @@ class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(i
         if (other.javaClass != javaClass) return false
 
         val o = other as EqualRange<T>
+        if (o.orientation != orientation) return false
         if (!equalRange(o.from, o.to)) return false
 
         return true
@@ -118,6 +125,7 @@ class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(i
         var hash = 7
         hash = 31 * hash + from
         hash = 31 * hash + to
+        hash = 31 * hash + orientation.ordinal
         return hash
     }
 
