343d88bb568bb33cb0b1992eb363d0f5d6937148
Maxim Alov macbook <orcchg@gmail.com>
2019-03-29 17:17:30 +0300

    Init joda time and get timezone

--- utility/src/main/kotlin/com/ringoid/utility/manager/TimezoneManager.kt
+++ utility/src/main/kotlin/com/ringoid/utility/manager/TimezoneManager.kt
@@ -1,10 +1,17 @@
 package com.ringoid.utility.manager
 
 import android.content.Context
+import net.danlew.android.joda.JodaTimeAndroid
+import org.joda.time.DateTimeZone
+import java.util.*
 
 class TimezoneManager(context: Context) {
 
+    init {
+        JodaTimeAndroid.init(context)
+    }
+
     /* API */
     // --------------------------------------------------------------------------------------------
-    fun getTimeZone(): Int = 0
+    fun getTimeZone(): Int = DateTimeZone.forTimeZone(TimeZone.getDefault()).getOffset(null) / 3600_000
 }
