76fc41d70803159f3a4562ca49d8b1327141c77d
Maxim Alov macbook <orcchg@gmail.com>
2019-04-17 15:37:57 +0300

    Basic insertion and read of feed items from db

--- data/src/main/kotlin/com/ringoid/data/local/database/dao/feed/FeedDao.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/dao/feed/FeedDao.kt
@@ -1,6 +1,8 @@
 package com.ringoid.data.local.database.dao.feed
 
 import androidx.room.*
+import com.ringoid.data.local.database.model.feed.FeedItemDbo
+import com.ringoid.data.local.database.model.feed.FullFeedItemDbo
 import com.ringoid.data.local.database.model.feed.ProfileDbo
 import com.ringoid.data.local.database.model.feed.ProfileWithImagesDbo
 import com.ringoid.data.local.database.model.image.ImageDbo
@@ -18,4 +20,12 @@ interface FeedDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addProfileImages(images: Collection<ImageDbo>)
+
+    // ------------------------------------------
+    @Transaction
+    @Query("SELECT * FROM ${FeedItemDbo.TABLE_NAME}")
+    fun feedItems(): Observable<List<FullFeedItemDbo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addFeedItems(feedItems: Collection<FeedItemDbo>)
 }
