660cd0c2819115987fd9df9f719aaa6ed9cf88a6
Maxim Alov macbook <orcchg@gmail.com>
2019-05-09 01:04:24 +0300

    #505: Dont purge Explore Feed on refresh Lmm / Profile

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreViewModel.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreViewModel.kt
@@ -2,8 +2,6 @@ package com.ringoid.origin.feed.view.explore
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import com.ringoid.base.eventbus.Bus
-import com.ringoid.base.eventbus.BusEvent
 import com.ringoid.base.view.IListScrollCallback
 import com.ringoid.base.view.ViewState
 import com.ringoid.domain.DomainUtil
@@ -14,14 +12,11 @@ import com.ringoid.domain.interactor.base.Params
 import com.ringoid.domain.interactor.feed.*
 import com.ringoid.domain.interactor.image.CountUserImagesUseCase
 import com.ringoid.domain.interactor.messenger.ClearMessagesForChatUseCase
-import com.ringoid.domain.log.SentryUtil
 import com.ringoid.domain.memory.IUserInMemoryCache
 import com.ringoid.domain.model.feed.Feed
 import com.ringoid.origin.feed.view.FeedViewModel
 import com.ringoid.origin.utils.ScreenHelper
 import com.uber.autodispose.lifecycle.autoDisposable
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -49,23 +44,6 @@ class ExploreViewModel @Inject constructor(
     private var nextPage: Int = 0
 
     override fun getFeedName(): String = DomainUtil.SOURCE_FEED_EXPLORE
-
-    // --------------------------------------------------------------------------------------------
-    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
-    fun onEventRefreshOnLmm(event: BusEvent.RefreshOnLmm) {
-        Timber.d("Received bus event: $event")
-        SentryUtil.breadcrumb("Bus Event", "event" to "$event")
-        // refresh on Lmm screen leads Feed screen to purge
-        clearScreen(ViewState.CLEAR.MODE_NEED_REFRESH)
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
-    fun onEventRefreshOnProfile(event: BusEvent.RefreshOnProfile) {
-        Timber.d("Received bus event: $event")
-        SentryUtil.breadcrumb("Bus Event", "event" to "$event")
-        // refresh on Profile screen leads Feed screen to purge
-        clearScreen(ViewState.CLEAR.MODE_NEED_REFRESH)
-    }
 
     // --------------------------------------------------------------------------------------------
     override fun getFeed() {
