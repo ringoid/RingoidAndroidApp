4ad4af70b6c6081cba08dbfa47924d41ecd34955
Maxim Alov macbook <orcchg@gmail.com>
2019-05-15 14:06:43 +0300

    #496: Process animation visual effect, create ImageView and play animation on it

--- origin/src/main/kotlin/com/ringoid/origin/view/common/visual/VisualAnimation.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/common/visual/VisualAnimation.kt
@@ -3,23 +3,32 @@ package com.ringoid.origin.view.common.visual
 import android.view.animation.*
 
 fun alphaIn(fromAlpha: Float, toAlpha: Float, dur: Long = 250L, offset: Long = 0L, interp: Interpolator? = null): Animation =
-    AlphaAnimation(fromAlpha, toAlpha).apply {
-        duration = dur
-        startOffset = offset
-        interp?.let { interpolator = it }
-    }
+    AlphaAnimation(fromAlpha, toAlpha)
+        .apply {
+            duration = dur
+            startOffset = offset
+            interp?.let { interpolator = it }
+        }
 
 fun alphaOut(fromAlpha: Float, toAlpha: Float, dur: Long = 80L, offset: Long = 0L, interp: Interpolator? = null): Animation =
-    AlphaAnimation(fromAlpha, toAlpha).apply {
-        duration = dur
-        startOffset = offset
-        interpolator = interp
-    }
+    AlphaAnimation(fromAlpha, toAlpha)
+        .apply {
+            duration = dur
+            startOffset = offset
+            interpolator = interp
+        }
 
 fun scaleUp(from: Float = 0.5f, dur: Long = 250L, interp: Interpolator? = null): Animation =
     ScaleAnimation(from, 1f, from, 1f,
         Animation.RELATIVE_TO_SELF, 0.5f,
         Animation.RELATIVE_TO_SELF, 0.5f)
+        .apply {
+            duration = dur
+            interp?.let { interpolator = it }
+        }
+
+fun translateUp(x: Float, y: Float, dist: Float, dur: Long = 250L, interp: Interpolator? = null): Animation =
+    TranslateAnimation(x, x, y, y - dist)
         .apply {
             duration = dur
             interp?.let { interpolator = it }
--- origin/src/main/kotlin/com/ringoid/origin/view/common/visual/VisualEffectView.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/common/visual/VisualEffectView.kt
@@ -2,7 +2,9 @@ package com.ringoid.origin.view.common.visual
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.animation.*
 import android.widget.FrameLayout
+import android.widget.ImageView
 import com.uber.autodispose.AutoDispose
 import com.uber.autodispose.android.scope
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -27,9 +29,29 @@ class VisualEffectView : FrameLayout {
         VisualEffectManager.effect
             .observeOn(AndroidSchedulers.mainThread())
             .`as`(AutoDispose.autoDisposable(scope()))
-            .subscribe({
-                Timber.v("VisualEffect: $it")
-                // TODO: play animation
-            }, Timber::e)
+            .subscribe({ playAnimation(it) }, Timber::e)
+    }
+
+    // ------------------------------------------
+    private fun playAnimation(effect: VisualEffect) {
+        Timber.v("VisualEffect: $effect")
+        val image = ImageView(context)
+            .apply {
+                layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT)
+                setImageResource(effect.resId)
+            }
+        val animationSet = AnimationSet(false)
+            .apply {
+                addAnimation(alphaIn(0.5f, 0.9f))
+                addAnimation(translateUp(effect.x.toFloat(), effect.y.toFloat(), 350f, 300L))
+                addAnimation(scaleUp(interp = AccelerateInterpolator()))
+                addAnimation(alphaOut(0.9f, 0f, offset = 300L, interp = DecelerateInterpolator()))
+                setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation) { addView(image) }
+                    override fun onAnimationRepeat(animation: Animation) { /* no-op */ }
+                    override fun onAnimationEnd(animation: Animation) { removeView(image) }
+                })
+            }
+        image.startAnimation(animationSet)
     }
 }
