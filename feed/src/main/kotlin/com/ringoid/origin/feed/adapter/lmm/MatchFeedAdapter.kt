044915cfd08d56d2450fc89d9ff6dd0a07d98bcb
Maxim Alov macbook <orcchg@gmail.com>
2019-05-21 10:56:17 +0300

    Organize imports

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/MatchFeedAdapter.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/MatchFeedAdapter.kt
@@ -1,11 +1,9 @@
 package com.ringoid.origin.feed.adapter.lmm
 
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.ringoid.origin.feed.adapter.base.BaseFeedViewHolder
 import com.ringoid.origin.feed.adapter.base.OriginFeedViewHolder
 import com.ringoid.origin.feed.model.ProfileImageVO
-import kotlinx.android.synthetic.main.rv_item_lmm_profile.view.*
 
 open class MatchFeedAdapter : BaseLmmAdapter() {
 
