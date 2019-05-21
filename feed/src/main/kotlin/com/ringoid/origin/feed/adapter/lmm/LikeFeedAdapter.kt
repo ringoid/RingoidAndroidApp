c252d3694d60439e84223aac96108fcc875c0f5e
Maxim Alov macbook <orcchg@gmail.com>
2019-05-21 09:54:54 +0300

    Revert "DELETE extra like image listener"
    This reverts commit a5bf4125e1e67e33545b01bbcb41e58734e892fa.

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/LikeFeedAdapter.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/lmm/LikeFeedAdapter.kt
@@ -2,14 +2,20 @@ package com.ringoid.origin.feed.adapter.lmm
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.ringoid.origin.feed.model.ProfileImageVO
+import com.ringoid.utility.changeVisibility
+import kotlinx.android.synthetic.main.rv_item_lmm_profile.view.*
 
 class LikeFeedAdapter : BaseLmmAdapter() {
 
+    var onLikeImageListener: ((model: ProfileImageVO, feedItemPosition: Int) -> Unit)? = null
+
     override fun instantiateViewHolder(view: View): LmmViewHolder =
         LikeFeedViewHolder(view, viewPool = imagesViewPool).also { vh ->
-            vh.profileImageAdapter.itemClickListener = { model, _ ->
+            vh.profileImageAdapter.itemClickListener = { model, position ->
                 if (vh.adapterPosition != RecyclerView.NO_POSITION) {
                     getModel(vh.adapterPosition).likedImages[model.image.id] = model.isLiked
+                    wrapOnImageClickListenerByFeedItem(vh, onLikeImageListener)?.invoke(model, position)
                 }
             }
         }
