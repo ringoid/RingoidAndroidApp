2fe08b3ee4660c6d3cf8ef688b2ba1dfc2f32500
Maxim Alov macbook <orcchg@gmail.com>
2019-05-31 14:29:10 +0300

    Rename model

--- data/src/main/kotlin/com/ringoid/data/local/database/UserRingoidDatabase.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/UserRingoidDatabase.kt
@@ -2,24 +2,20 @@ package com.ringoid.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.ringoid.data.local.database.dao.feed.UserFeedDao
 import com.ringoid.data.local.database.dao.image.ImageRequestDao
 import com.ringoid.data.local.database.dao.image.UserImageDao
 import com.ringoid.data.local.database.dao.messenger.MessageDao
 import com.ringoid.data.local.database.dao.user.UserDao
-import com.ringoid.data.local.database.model.feed.ProfileDbo
+import com.ringoid.data.local.database.model.feed.UserProfileDbo
 import com.ringoid.data.local.database.model.feed.ProfileIdDbo
-import com.ringoid.data.local.database.model.image.ImageDbo
 import com.ringoid.data.local.database.model.image.ImageRequestDbo
 import com.ringoid.data.local.database.model.image.UserImageDbo
 import com.ringoid.data.local.database.model.messenger.MessageDbo
-import javax.inject.Inject
 
 @Database(version = 101,
           entities = [ImageRequestDbo::class, MessageDbo::class,
-                      ProfileDbo::class, ProfileIdDbo::class, UserImageDbo::class])
+                      ProfileIdDbo::class, UserImageDbo::class, UserProfileDbo::class])
 abstract class UserRingoidDatabase : RoomDatabase() {
 
     companion object {
--- data/src/main/kotlin/com/ringoid/data/local/database/model/feed/UserProfileDbo.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/model/feed/UserProfileDbo.kt
@@ -4,8 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = ProfileDbo.TABLE_NAME)
-data class ProfileDbo(@PrimaryKey @ColumnInfo(name = COLUMN_ID, index = true) val id: String) {
+@Entity(tableName = UserProfileDbo.TABLE_NAME)
+data class UserProfileDbo(@PrimaryKey @ColumnInfo(name = COLUMN_ID, index = true) val id: String) {
 
     companion object {
         const val COLUMN_ID = "id"
