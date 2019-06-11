8ce76d1e20f04caa4e157518158814f20c796598
Maxim Alov <orcchg@yandex.ru>
2019-06-11 11:41:10 +0300

    Organize imports

--- origin/src/main/kotlin/com/ringoid/origin/BaseAppGlideModule.kt
+++ origin/src/main/kotlin/com/ringoid/origin/BaseAppGlideModule.kt
@@ -1,14 +1,10 @@
 package com.ringoid.origin
 
 import android.content.Context
-import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
-import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool
-import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
-import java.io.InputStream
 
 @GlideModule
 class BaseAppGlideModule : AppGlideModule() {
