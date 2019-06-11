9653af37afad407011ba807c9ea3eedddfc5d5a3
Maxim Alov <orcchg@yandex.ru>
2019-06-11 11:30:26 +0300

    #618: Remove debug payload

--- feed/src/main/kotlin/com/ringoid/origin/feed/adapter/FeedAdapter.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/adapter/FeedAdapter.kt
@@ -1,14 +1,11 @@
 package com.ringoid.origin.feed.adapter
 
 import android.view.View
-import com.ringoid.domain.model.debug.VerticalDebugBlob
 import com.ringoid.origin.feed.R
 import com.ringoid.origin.feed.adapter.base.*
 import com.ringoid.origin.feed.model.EmptyFeedItemVO
 import com.ringoid.origin.feed.model.FeedItemVO
 import com.ringoid.origin.feed.model.ProfileImageVO
-import com.ringoid.utility.randomString
-import leakcanary.LeakSentry
 
 class FeedAdapter : BaseFeedAdapter(FeedItemVODiffCallback()) {
 
@@ -22,18 +19,10 @@ class FeedAdapter : BaseFeedAdapter(FeedItemVODiffCallback()) {
                 getModel(vh.adapterPosition).likedImages[model.image.id] = model.isLiked
                 onLikeImageListener?.invoke(model, position)
             }
-            vh.debugBlob = VerticalDebugBlob(tag = "${vh.adapterPosition}_${randomString(4)}")
         }
 
     override fun instantiateHeaderViewHolder(view: View) = HeaderFeedViewHolder(view)
     override fun instantiateFooterViewHolder(view: View) = FooterFeedViewHolder(view)
-
-    override fun onViewRecycled(holder: OriginFeedViewHolder) {
-        super.onViewRecycled(holder)
-        holder.takeIf { it is FeedViewHolder }
-              ?.let { it as FeedViewHolder }
-              ?.let { LeakSentry.refWatcher.watch(it.debugBlob, it.debugBlob.tag) }
-    }
 
     // ------------------------------------------
     override fun getStubItem(): FeedItemVO = EmptyFeedItemVO
