e6735215f717650c9501400107b7f6a047793341
Maxim Alov <orcchg@yandex.ru>
2019-06-19 13:03:56 +0300

    Fix unbounded threads creation in Schedulers.io()

--- data/src/main/kotlin/com/ringoid/data/local/database/dao/debug/DebugLogDaoHelper.kt
+++ data/src/main/kotlin/com/ringoid/data/local/database/dao/debug/DebugLogDaoHelper.kt
@@ -1,15 +1,11 @@
 package com.ringoid.data.local.database.dao.debug
 
-import com.ringoid.data.local.database.model.debug.DebugLogItemDbo
 import com.ringoid.domain.debug.DebugLogItem
 import com.ringoid.domain.debug.IDebugLogDaoHelper
 import com.ringoid.domain.model.mapList
-import io.reactivex.Completable
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Singleton
-import timber.log.Timber
 
 @Singleton
 class DebugLogDaoHelper @Inject constructor(private val dao: DebugLogDao) : IDebugLogDaoHelper {
@@ -18,15 +14,15 @@ class DebugLogDaoHelper @Inject constructor(private val dao: DebugLogDao) : IDeb
 
     @Suppress("CheckResult")
     override fun addDebugLog(log: DebugLogItem) {
-        Completable.fromCallable { dao.addDebugLog(DebugLogItemDbo.from(log)) }
-            .subscribeOn(Schedulers.io())
-            .subscribe({}, Timber::e)
+//        Completable.fromCallable { dao.addDebugLog(DebugLogItemDbo.from(log)) }
+//            .subscribeOn(Schedulers.io())
+//            .subscribe({}, Timber::e)
     }
 
     @Suppress("CheckResult")
     override fun deleteDebugLog() {
-        Completable.fromCallable { dao.deleteDebugLog() }
-            .subscribeOn(Schedulers.io())
-            .subscribe({}, Timber::e)
+//        Completable.fromCallable { dao.deleteDebugLog() }
+//            .subscribeOn(Schedulers.io())
+//            .subscribe({}, Timber::e)
     }
 }
