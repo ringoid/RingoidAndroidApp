b0f04f4b3b47040b84e2ec2c30e377d2d8dee8c7
Maxim Alov <orcchg@yandex.ru>
2019-06-28 15:49:58 +0300

    #657: Fill custom properties and send them to backend

--- origin/src/main/kotlin/com/ringoid/origin/model/UserProfileProperties.kt
+++ origin/src/main/kotlin/com/ringoid/origin/model/UserProfileProperties.kt
@@ -11,7 +11,16 @@ data class UserProfileProperties(
     var height: Int = DomainUtil.UNKNOWN_VALUE,
     var income: IncomeProfileProperty = IncomeProfileProperty.Unknown,
     var property: PropertyProfileProperty = PropertyProfileProperty.Unknown,
-    var transport: TransportProfileProperty = TransportProfileProperty.Unknown)
+    var transport: TransportProfileProperty = TransportProfileProperty.Unknown,
+    var about: String = "",
+    var company: String = "",
+    var jobTitle: String = "",
+    var name: String = "",
+    var socialInstagram: String = "",
+    var socialTikTok: String = "",
+    var university: String = "",
+    var whereFrom: String = "",
+    var whereLive: String = "")
     : Mappable<UserProfilePropertiesRaw> {
 
     companion object {
@@ -23,7 +32,16 @@ data class UserProfileProperties(
                 height = raw.height,
                 income = IncomeProfileProperty.from(raw.income),
                 property = PropertyProfileProperty.from(raw.property),
-                transport = TransportProfileProperty.from(raw.transport))
+                transport = TransportProfileProperty.from(raw.transport),
+                about = raw.about,
+                company = raw.company,
+                jobTitle = raw.jobTitle,
+                name = raw.name,
+                socialInstagram = raw.socialInstagram,
+                socialTikTok = raw.socialTikTok,
+                university = raw.university,
+                whereFrom = raw.whereFrom,
+                whereLive = raw.whereLive)
     }
 
     fun isAllUnknown(): Boolean =
@@ -33,7 +51,16 @@ data class UserProfileProperties(
         height == DomainUtil.UNKNOWN_VALUE &&
         income == IncomeProfileProperty.Unknown &&
         property == PropertyProfileProperty.Unknown &&
-        transport == TransportProfileProperty.Unknown
+        transport == TransportProfileProperty.Unknown &&
+        about.isBlank() &&
+        company.isBlank() &&
+        jobTitle.isBlank() &&
+        name.isBlank() &&
+        socialInstagram.isBlank() &&
+        socialTikTok.isBlank() &&
+        university.isBlank() &&
+        whereFrom.isBlank() &&
+        whereLive.isBlank()
 
     override fun map(): UserProfilePropertiesRaw =
         UserProfilePropertiesRaw(
@@ -43,5 +70,14 @@ data class UserProfileProperties(
             height = height,
             income = income.id,
             property = property.id,
-            transport = transport.id)
+            transport = transport.id,
+            about = about,
+            company = company,
+            jobTitle = jobTitle,
+            name = name,
+            socialInstagram = socialInstagram,
+            socialTikTok = socialTikTok,
+            university = university,
+            whereFrom = whereFrom,
+            whereLive = whereLive)
 }
