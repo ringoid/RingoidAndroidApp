2a01b195bfaf39298595c5feca8037ebb24ae0d9
Maxim Alov <orcchg@yandex.ru>
2019-07-27 11:10:53 +0300

    Methods

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/base/BaseLcFeedFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/base/BaseLcFeedFragment.kt
@@ -1,7 +1,15 @@
 package com.ringoid.origin.feed.view.lc.base
 
+import com.ringoid.origin.feed.OriginR_string
+import com.ringoid.origin.feed.adapter.lmm.BaseLmmAdapter
 import com.ringoid.origin.feed.view.FeedFragment
 
 abstract class BaseLcFeedFragment<VM : BaseLcFeedViewModel> : FeedFragment<VM>() {
 
+    abstract fun instantiateFeedAdapter(): BaseLmmAdapter
+
+    override fun createFeedAdapter(): BaseLmmAdapter =
+        instantiateFeedAdapter()
+
+    override fun getAddPhotoDialogDescriptionResId(): Int = OriginR_string.feed_lmm_dialog_no_user_photo_description
 }
--- feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/like/LikesFeedFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/like/LikesFeedFragment.kt
@@ -1,6 +1,14 @@
 package com.ringoid.origin.feed.view.lc.like
 
+import com.ringoid.base.view.ViewState
+import com.ringoid.origin.feed.OriginR_string
+import com.ringoid.origin.feed.adapter.lmm.BaseLmmAdapter
+import com.ringoid.origin.feed.adapter.lmm.LikeFeedAdapter
+import com.ringoid.origin.feed.model.ProfileImageVO
 import com.ringoid.origin.feed.view.lc.base.BaseLcFeedFragment
+import com.ringoid.origin.navigation.noConnection
+import com.ringoid.origin.view.common.EmptyFragment
+import com.ringoid.utility.image.ImageRequest
 
 class LikesFeedFragment : BaseLcFeedFragment<LikesFeedViewModel>() {
 
@@ -9,4 +17,24 @@ class LikesFeedFragment : BaseLcFeedFragment<LikesFeedViewModel>() {
     }
 
     override fun getVmClass(): Class<LikesFeedViewModel> = LikesFeedViewModel::class.java
+
+    override fun instantiateFeedAdapter(): BaseLmmAdapter =
+        LikeFeedAdapter(ImageRequest(context!!)).apply {
+            onLikeImageListener = { model: ProfileImageVO, _ /** feed item position */: Int ->
+                if (!connectionManager.isNetworkAvailable()) {
+                    noConnection(this@LikesFeedFragment)
+                } else {
+                    vm.onLike(profileId = model.profileId, imageId = model.image.id)
+                }
+            }
+        }
+
+    override fun getEmptyStateInput(mode: Int): EmptyFragment.Companion.Input? =
+        when (mode) {
+            ViewState.CLEAR.MODE_EMPTY_DATA -> EmptyFragment.Companion.Input(emptyTextResId = OriginR_string.feed_likes_you_empty_no_data)
+            ViewState.CLEAR.MODE_NEED_REFRESH -> EmptyFragment.Companion.Input(emptyTextResId = OriginR_string.common_pull_to_refresh)
+            else -> null
+        }
+
+    // --------------------------------------------------------------------------------------------
 }
