560bdbe7b9ef3590a2fc650cb0d7dc6f5c4e1c82
Maxim Alov <orcchg@yandex.ru>
2019-07-31 22:34:58 +0300

    On Chat close - scroll list to top of corresponding feed item to make it in view focus

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/messenger/MessagesFeedFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/messenger/MessagesFeedFragment.kt
@@ -138,8 +138,9 @@ class MessagesFeedFragment : BaseLcFeedFragment<MessagesFeedViewModel>(), IChatH
                 when (tag) {
                     ChatFragment.TAG -> {
                         // TODO: supply firstUserMessage to change appearance
-                        feedAdapter.findModel(it.position)?.setOnlineStatus(it.onlineStatus)
+                        scrollToTopOfItemAtPosition(it.position)
                         getRecyclerView().post {
+                            feedAdapter.findModel(it.position)?.setOnlineStatus(it.onlineStatus)
                             feedAdapter.notifyItemChanged(it.position, FeedViewHolderShowControls)
                             trackScrollOffsetForPosition(it.position)
                         }
