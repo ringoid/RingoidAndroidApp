2ecaf636711d73373c052a02398346ab7d2d0724
Maxim Alov <orcchg@yandex.ru>
2019-08-01 20:20:51 +0300

    Fix typo

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/lmm/base/BaseMatchesFeedScreenResiduals.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/lmm/base/BaseMatchesFeedScreenResiduals.kt
@@ -5,5 +5,5 @@ import com.ringoid.base.view.Residual
 data class PUSH_NEW_MESSAGES(val profileId: String) : Residual()
 
 object PUSH_NEW_LIKES_TOTAL : Residual()
-object PUSH_NEW_MATCHES_TOTAL: Residual()
+object PUSH_NEW_MATCHES_TOTAL : Residual()
 object PUSH_NEW_MESSAGES_TOTAL : Residual()
