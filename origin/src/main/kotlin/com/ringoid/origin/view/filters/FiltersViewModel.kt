23c75ca49a937f195c6d9ca20ebbcb415e5e5a9d
Maxim Alov <orcchg@yandex.ru>
2019-08-05 13:18:23 +0300

    Provide filters viewModel

--- origin/src/main/kotlin/com/ringoid/origin/view/filters/FiltersViewModel.kt
+++ origin/src/main/kotlin/com/ringoid/origin/view/filters/FiltersViewModel.kt
@@ -2,5 +2,6 @@ package com.ringoid.origin.view.filters
 
 import android.app.Application
 import com.ringoid.base.viewmodel.BaseViewModel
+import javax.inject.Inject
 
-class FiltersViewModel(app: Application) : BaseViewModel(app)
+class FiltersViewModel @Inject constructor(app: Application) : BaseViewModel(app)
