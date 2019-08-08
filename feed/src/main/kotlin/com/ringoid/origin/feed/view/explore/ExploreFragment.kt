cff764827a3a9c73444db05f71771202fa2488b5
Maxim Alov <orcchg@yandex.ru>
2019-08-08 09:12:58 +0300

    Remove title from empty Explore screen as it overlaps with toolbar title for filters

--- feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreFragment.kt
+++ feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreFragment.kt
@@ -52,9 +52,9 @@ class ExploreFragment : FeedFragment<ExploreViewModel>() {
 
     override fun getEmptyStateInput(mode: Int): EmptyFragment.Companion.Input? =
         when (mode) {
-            ViewState.CLEAR.MODE_EMPTY_DATA -> EmptyFragment.Companion.Input(emptyTitleResId = OriginR_string.feed_explore_empty_title, emptyTextResId = OriginR_string.feed_explore_empty_no_data)
-            ViewState.CLEAR.MODE_NEED_REFRESH -> EmptyFragment.Companion.Input(emptyTitleResId = OriginR_string.feed_explore_empty_title, emptyTextResId = OriginR_string.common_pull_to_refresh)
-            ViewState.CLEAR.MODE_CHANGE_FILTERS -> EmptyFragment.Companion.Input(emptyTitleResId = OriginR_string.feed_explore_empty_title, emptyTextResId = OriginR_string.feed_empty_no_data_filters)
+            ViewState.CLEAR.MODE_EMPTY_DATA -> EmptyFragment.Companion.Input(emptyTextResId = OriginR_string.feed_explore_empty_no_data)
+            ViewState.CLEAR.MODE_NEED_REFRESH -> EmptyFragment.Companion.Input(emptyTextResId = OriginR_string.common_pull_to_refresh)
+            ViewState.CLEAR.MODE_CHANGE_FILTERS -> EmptyFragment.Companion.Input(emptyTextResId = OriginR_string.feed_empty_no_data_filters)
             else -> null
         }
 
