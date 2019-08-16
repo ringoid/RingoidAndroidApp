b112997ee2656059d59cb8454392c48b4e615e8d
Maxim Alov <orcchg@yandex.ru>
2019-08-16 12:16:32 +0300

    #811: Start implementing facade for remote logic

---
+++ library/data/src/main/kotlin/com/ringoid/data/remote/facade/SystemCloudFacadeImpl.kt
@@ -0,0 +1,6 @@
+package com.ringoid.data.remote.facade
+
+import com.ringoid.data.remote.api.SystemCloud
+import javax.inject.Inject
+
+class SystemCloudFacadeImpl @Inject constructor(private val clou: SystemCloud)
