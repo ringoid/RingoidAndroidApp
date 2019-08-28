abdb3c6016340244c3223b02b1cfa86e98ea1759
Maxim Alov <orcchg@yandex.ru>
2019-08-28 19:49:50 +0300

    Move class

---
+++ feature/feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/FeedCounts.kt
@@ -0,0 +1,3 @@
+package com.ringoid.origin.feed.view.lc
+
+data class FeedCounts(val show: Int, val hidden: Int)
--- feature/feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/LcScreenResiduals.kt
+++ feature/feed/src/main/kotlin/com/ringoid/origin/feed/view/lc/LcScreenResiduals.kt
@@ -2,8 +2,6 @@ package com.ringoid.origin.feed.view.lc
 
 import com.ringoid.base.view.Residual
 
-data class FeedCounts(val show: Int, val hidden: Int)
-
 data class SEEN_ALL_FEED(val sourceFeed: Int) : Residual() {
     companion object {
         const val FEED_LIKES = 0
