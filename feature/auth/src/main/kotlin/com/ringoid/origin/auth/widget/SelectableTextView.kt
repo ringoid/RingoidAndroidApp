8c22b024c60091002f59be90d530606be767efc0
Maxim Alov <orcchg@yandex.ru>
2019-08-29 11:02:28 +0300

    Use correct way to save and restore View state

--- feature/auth/src/main/kotlin/com/ringoid/origin/auth/widget/SelectableTextView.kt
+++ feature/auth/src/main/kotlin/com/ringoid/origin/auth/widget/SelectableTextView.kt
@@ -1,17 +1,18 @@
 package com.ringoid.origin.auth.widget
 
 import android.content.Context
-import android.os.Bundle
+import android.os.Parcel
 import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.TextView
 
+/**
+ * For saving View state:
+ *
+ * @see https://trickyandroid.com/saving-android-view-state-correctly/?source=post_page-----9dbe96074d49----------------------
+ * @see https://medium.com/@kirillsuslov/how-to-save-android-view-state-in-kotlin-9dbe96074d49
+ */
 class SelectableTextView : TextView {
-
-    companion object {
-        private const val BUNDLE_KEY_FLAG_IS_SELECTED = "bundle_key_flag_is_selected"
-        private const val BUNDLE_KEY_SUPER_STATE = "superState"
-    }
 
     constructor(context: Context) : this(context, null)
 
@@ -21,18 +22,43 @@ class SelectableTextView : TextView {
         isClickable = true
     }
 
-    override fun onRestoreInstanceState(state: Parcelable) {
-        val xstate = state.let { it as? Bundle }
-            ?.let {
-                isSelected = it.getBoolean(BUNDLE_KEY_FLAG_IS_SELECTED, false)
-                it.getParcelable(BUNDLE_KEY_SUPER_STATE) as Parcelable
-            } ?: state
-        super.onRestoreInstanceState(xstate)
-    }
+    override fun onRestoreInstanceState(state: Parcelable) =
+        if (state is SavedState) {
+            super.onRestoreInstanceState(state.superState)
+            this@SelectableTextView.isSelected = state.isSelected
+        } else {
+            super.onRestoreInstanceState(state)
+        }
 
     override fun onSaveInstanceState(): Parcelable? =
-        Bundle().apply {
-            putParcelable(BUNDLE_KEY_SUPER_STATE, super.onSaveInstanceState())
-            putBoolean(BUNDLE_KEY_FLAG_IS_SELECTED, isSelected)
+        super.onSaveInstanceState()?.let {
+            SavedState(it).apply {
+                isSelected = this@SelectableTextView.isSelected
+            }
         }
+
+    // --------------------------------------------------------------------------------------------
+    internal class SavedState : androidx.customview.view.AbsSavedState {
+
+        var isSelected: Boolean = false
+
+        constructor(superState: Parcelable) : super(superState)
+
+        constructor(source: Parcel) : super(source) {
+            isSelected = source.readByte().toInt() != 0
+        }
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            super.writeToParcel(dest, flags)  // avoid ClassNotFoundException when unmarshalling: <some name>
+            dest.writeByte((if (isSelected) 1 else 0).toByte())
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState = SavedState(source)
+                override fun newArray(size: Int): Array<SavedState?> = arrayOfNulls(size)
+            }
+        }
+    }
 }
