72cda16c078462dc5a0941be1ec1f777951f9c10
Maxim Alov <orcchg@yandex.ru>
2019-09-03 05:50:47 +0300

    Revert "Refresh Explore feed on tab transaction after login and ignore app fresh start event on Explore feed"
    This reverts commit 88c6d43a66dcfec72d4c77c927d99beb733392cb.

--- feature/feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreFeedViewModel.kt
+++ feature/feed/src/main/kotlin/com/ringoid/origin/feed/view/explore/ExploreFeedViewModel.kt
@@ -232,6 +232,14 @@ class ExploreFeedViewModel @Inject constructor(
 
     // --------------------------------------------------------------------------------------------
     @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
+    fun onEventAppFreshStart(event: BusEvent.AppFreshStart) {
+        Timber.d("Received bus event: $event")
+        SentryUtil.breadcrumb("Bus Event ${event.javaClass.simpleName}", "event" to "$event")
+        DebugLogUtil.i("Get Discover on Application fresh start [${getFeedName()}]")
+        refresh()
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
     fun onEventRecreateMainScreen(event: BusEvent.RecreateMainScreen) {
         Timber.d("Received bus event: $event")
         SentryUtil.breadcrumb("Bus Event ${event.javaClass.simpleName}", "event" to "$event")
