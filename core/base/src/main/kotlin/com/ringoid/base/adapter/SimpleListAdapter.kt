685cacc940fd550f5aab41eee50b28853d1f0a61
Maxim Alov <orcchg@yandex.ru>
2019-09-05 15:15:11 +0300

    Add bufferred insert

--- core/base/src/main/kotlin/com/ringoid/base/adapter/SimpleListAdapter.kt
+++ core/base/src/main/kotlin/com/ringoid/base/adapter/SimpleListAdapter.kt
@@ -10,11 +10,16 @@ import com.ringoid.domain.model.IListModel
 abstract class SimpleListAdapter<T : IListModel, VH : BaseViewHolder<T>>(
     private val insertCb: ((total: Int) -> Unit)? = null) : RecyclerView.Adapter<VH>() {
 
+    companion object {
+        const val BUFFER_LIMIT = 60
+    }
+
     init {
         setHasStableIds(true)
     }
 
-    protected val models = mutableListOf<T>()
+    protected val models = ArrayList<T>(2000)
+    private val buffer = ArrayList<T>(BUFFER_LIMIT + 2)
 
     @LayoutRes protected abstract fun getLayoutId(): Int
     protected abstract fun instantiateViewHolder(view: View): VH
@@ -35,6 +40,18 @@ abstract class SimpleListAdapter<T : IListModel, VH : BaseViewHolder<T>>(
         models.add(item)
         notifyItemInserted(position)
         insertCb?.invoke(models.size)  /* total count */
+    }
+
+    fun appendBuffer(item: T) {
+        buffer.add(item)
+        if (buffer.size >= BUFFER_LIMIT) {
+            val position = models.size
+            models.addAll(buffer)
+            buffer.clear()
+//            notifyItemRangeInserted(position, buffer.size)
+            notifyDataSetChanged()
+            insertCb?.invoke(models.size)
+        }
     }
 
     fun clear() {
