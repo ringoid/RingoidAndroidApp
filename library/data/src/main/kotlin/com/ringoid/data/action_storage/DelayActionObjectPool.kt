76d18d482e5e16d8220f3ab7af2c6c18a2c10b6c
Maxim Alov <orcchg@yandex.ru>
2019-09-06 17:30:44 +0300

    Implement delayed action object pool

---
+++ library/data/src/main/kotlin/com/ringoid/data/action_storage/DelayActionObjectPool.kt
@@ -0,0 +1,43 @@
+package com.ringoid.data.action_storage
+
+import com.ringoid.data.local.shared_prefs.SharedPrefsManager
+import com.ringoid.datainterface.remote.IRingoidCloudFacade
+import com.ringoid.debug.DebugLogUtil
+import io.reactivex.Single
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
+
+abstract class DelayActionObjectPool(cloud: IRingoidCloudFacade, spm: SharedPrefsManager)
+    : BaseActionObjectPool(cloud, spm) {
+
+    companion object {
+        const val DELAY_TRIGGER = 200L  // ms
+    }
+
+    private val triggerInProgress = AtomicBoolean(false)
+
+    protected abstract fun triggerSourceImpl(): Single<Long>
+
+    /**
+     * Thread performs [triggerSourceImpl], others are delayed until the first one finishes.
+     * Delays thread for a short period of time, then check whether the job is still in progress,
+     * and if so - delay again, otherwise - start doing the job.
+     */
+    override fun triggerSource(): Single<Long> =
+        countActionObjects()
+            .flatMap { count ->
+                when {
+                    count <= 0 -> {  // no job to do for this thread
+                        DebugLogUtil.d("No actions to commit, lAt is up-to-date [chained]")
+                        Single.just(lastActionTime())
+                    }
+                    triggerInProgress.get() ->  // job is in progress by some other thread
+                        Single.just(0L)
+                              .delay(DELAY_TRIGGER, TimeUnit.MILLISECONDS)
+                              .flatMap { triggerSource() }
+                    else -> triggerSourceImpl()  // this thread should do the job
+                        .doOnSubscribe { triggerInProgress.set(true) }
+                        .doFinally { triggerInProgress.set(false) }
+                }
+            }
+}
