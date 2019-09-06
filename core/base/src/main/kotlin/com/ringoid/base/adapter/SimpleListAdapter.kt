49901c14de174046560597b7ad1acc3bb6b4f7b8
Maxim Alov <orcchg@yandex.ru>
2019-09-06 20:41:26 +0300

    Implement bufferred debug logs adapter

--- core/base/src/main/kotlin/com/ringoid/base/adapter/SimpleListAdapter.kt
+++ core/base/src/main/kotlin/com/ringoid/base/adapter/SimpleListAdapter.kt
@@ -8,18 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ringoid.domain.model.IListModel
 
 abstract class SimpleListAdapter<T : IListModel, VH : BaseViewHolder<T>>(
-    private val insertCb: ((total: Int) -> Unit)? = null) : RecyclerView.Adapter<VH>() {
-
-    companion object {
-        const val BUFFER_LIMIT = 60
-    }
+    protected val insertCb: ((total: Int) -> Unit)? = null) : RecyclerView.Adapter<VH>() {
 
     init {
         setHasStableIds(true)
     }
 
     protected val models = ArrayList<T>(2000)
-    private val buffer = ArrayList<T>(BUFFER_LIMIT + 2)
 
     @LayoutRes protected abstract fun getLayoutId(): Int
     protected abstract fun instantiateViewHolder(view: View): VH
@@ -35,23 +30,11 @@ abstract class SimpleListAdapter<T : IListModel, VH : BaseViewHolder<T>>(
 
     override fun getItemCount(): Int = models.size
 
-    fun append(item: T) {
+    open fun append(item: T) {
         val position = models.size
         models.add(item)
         notifyItemInserted(position)
         insertCb?.invoke(models.size)  /* total count */
-    }
-
-    fun appendBuffer(item: T) {
-        buffer.add(item)
-        if (buffer.size >= BUFFER_LIMIT) {
-            val position = models.size
-            models.addAll(buffer)
-            buffer.clear()
-//            notifyItemRangeInserted(position, buffer.size)
-            notifyDataSetChanged()
-            insertCb?.invoke(models.size)
-        }
     }
 
     fun clear() {
--- feature/origin/src/main/kotlin/com/ringoid/origin/view/debug/SimpleDebugLogItemAdapter.kt
+++ feature/origin/src/main/kotlin/com/ringoid/origin/view/debug/SimpleDebugLogItemAdapter.kt
@@ -7,10 +7,28 @@ import com.ringoid.origin.R
 class SimpleDebugLogItemAdapter(insertCb: ((total: Int) -> Unit)? = null)
     : SimpleListAdapter<DebugLogItemVO, OriginDebugLogItemViewHolder>(insertCb) {
 
+    companion object {
+        const val BUFFER_LIMIT = 60
+    }
+
+    private val buffer = ArrayList<DebugLogItemVO>(BUFFER_LIMIT + 2)
+
     override fun getLayoutId(): Int = R.layout.rv_item_debug_log
 
     override fun instantiateViewHolder(view: View) = DebugLogItemViewHolder(view)
 
     // ------------------------------------------
     fun getContentText(): String = models.joinToString("\n", transform = { it.log.log() })
+
+    override fun append(item: DebugLogItemVO) {
+        buffer.add(item)
+        if (buffer.size >= BUFFER_LIMIT) {
+            val position = models.size
+            buffer.sortBy { it.log.tick }
+            models.addAll(buffer)
+            buffer.clear()
+            notifyItemRangeInserted(position, buffer.size)
+            insertCb?.invoke(models.size)  // total size
+        }
+    }
 }
