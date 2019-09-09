61c5a10de3d42ae6f78c03734be5436060b0f4f4
Maxim Alov <orcchg@yandex.ru>
2019-09-09 18:12:03 +0300

    If user has typed some uri instead of social user id, don't prefix it with 'at @' char

--- library/utility/src/main/kotlin/com/ringoid/utility/ValueUtils.kt
+++ library/utility/src/main/kotlin/com/ringoid/utility/ValueUtils.kt
@@ -1,13 +1,19 @@
 package com.ringoid.utility
 
+import android.webkit.URLUtil
+
 object ValueUtils {
 
     fun isValidLocation(latitude: Double, longitude: Double): Boolean =
         latitude != 0.0 || longitude != 0.0
 
     fun atCharSocialId(socialId: String): String =
-        socialId.firstOrNull()
-            ?.let { c -> c.takeIf { it == '@' }?.let { socialId } ?: "@$socialId" }
-            ?.trim()
-            ?: ""
+        if (URLUtil.isValidUrl(socialId)) {
+            socialId  // don't prefix uri
+        } else {
+            socialId.firstOrNull()
+                ?.let { c -> c.takeIf { it == '@' }?.let { socialId } ?: "@$socialId" }
+                ?.trim()
+                ?: ""
+        }
 }
