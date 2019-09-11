bda386c38b4ddb88884006b56286b7da9f904f7d
Maxim Alov <orcchg@yandex.ru>
2019-09-11 10:06:23 +0300

    #930: Make section containers and about label clickable on feed items, opening context menu, same as by clicking on settings button on feed item

--- feature/feed/src/main/kotlin/com/ringoid/origin/feed/adapter/base/BaseFeedAdapter.kt
+++ feature/feed/src/main/kotlin/com/ringoid/origin/feed/adapter/base/BaseFeedAdapter.kt
@@ -54,8 +54,12 @@ abstract class BaseFeedAdapter(diffCb: BaseDiffCallback<FeedItemVO>, headerRows:
                 vh.trackingBus = this@BaseFeedAdapter.trackingBus
                 val wrapSettingsClickListener: ((model: FeedItemVO, position: Int) -> Unit)? =
                     { model: FeedItemVO, position: Int -> settingsClickListener?.invoke(model, position, vh.getCurrentImagePosition()) }
+                val wrapWrapSettingsClickListener = wrapOnItemClickListener(vh, wrapSettingsClickListener)
                 vh.itemView.ibtn_settings.clicks().compose(clickDebounce())
-                    .subscribe { wrapOnItemClickListener(vh, wrapSettingsClickListener).onClick(vh.itemView.ibtn_settings) }
+                    .subscribe { wrapWrapSettingsClickListener.onClick(vh.itemView.ibtn_settings) }
+                vh.itemView.tv_about.clicks().compose(clickDebounce()).subscribe { wrapWrapSettingsClickListener.onClick(vh.itemView.tv_about) }
+                vh.itemView.ll_left_container.clicks().compose(clickDebounce()).subscribe { wrapWrapSettingsClickListener.onClick(vh.itemView.ll_left_container) }
+                vh.itemView.ll_right_container.clicks().compose(clickDebounce()).subscribe { wrapWrapSettingsClickListener.onClick(vh.itemView.ll_right_container) }
                 vh.setOnClickListener(null)  // clicks on the whole feed's item is no-op
             }
             ?.takeIf { it is BaseFeedViewHolder }
