7b9a2d3bb907e9824e7beaef9687371210c860a2
Maxim Alov <orcchg@yandex.ru>
2019-09-12 11:23:11 +0300

    Add log id method

--- feature/feed/src/main/kotlin/com/ringoid/origin/feed/model/ProfileImageVO.kt
+++ feature/feed/src/main/kotlin/com/ringoid/origin/feed/model/ProfileImageVO.kt
@@ -8,6 +8,8 @@ import com.ringoid.utility.randomString
 data class ProfileImageVO(val profileId: String, val image: IImage) : IListModel {
 
     override fun getModelId(): Long = ((217L + image.getModelId()) * 31L) + profileId.hashCode()
+
+    fun idWithFirstN(N: Int = 3): String = profileId.substring(0..minOf(N, profileId.length - 1))
 }
 
 val EmptyProfileImageVO = ProfileImageVO(profileId = randomString(), image = EmptyImage)
