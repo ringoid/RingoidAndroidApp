c5138061b5dc4b6cde0810c79648266d7c04eab3
Maxim Alov <orcchg@yandex.ru>
2019-09-18 23:53:21 +0300

    #936: Display tick icons depending on read status for peer messages

--- feature/messenger/src/main/kotlin/com/ringoid/origin/messenger/res.kt
+++ feature/messenger/src/main/kotlin/com/ringoid/origin/messenger/res.kt
@@ -1,4 +1,5 @@
 package com.ringoid.origin.messenger
 
 typealias OriginR_string = com.ringoid.origin.R.string
+typealias WidgetR_drawable = com.ringoid.widget.R.drawable
 typealias WidgetR_style = com.ringoid.widget.R.style
