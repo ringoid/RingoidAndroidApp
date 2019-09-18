1612415db1bd85facbd776179b12d4133e5811c2
Maxim Alov <orcchg@yandex.ru>
2019-09-18 17:49:18 +0300

    Add new trigger strategy for action objects - NoAction - that won't apply any strategy that will trigger the Queue automatically, so it's up to some other strategy with some other action objects being put or some other trigger being called to perform commit actions, that will include the former action objects with NoAction strategy as well

--- core/domain/src/main/kotlin/com/ringoid/domain/action_storage/TriggerStrategy.kt
+++ core/domain/src/main/kotlin/com/ringoid/domain/action_storage/TriggerStrategy.kt
@@ -1,17 +1,39 @@
 package com.ringoid.domain.action_storage
 
+import com.ringoid.domain.model.actions.BaseActionObject
+
+/**
+ * [IActionObjectPool] will analyze [TriggerStrategy] for each incoming [BaseActionObject] and
+ * commit action objects when strategy fulfills.
+ */
 sealed class TriggerStrategy {
 
     override fun toString(): String = javaClass.simpleName
 }
 
+/**
+ * [IActionObjectPool] won't commit actions automatically while [BaseActionObject] with
+ * this strategy are put into, until some action object with another strategy comes.
+ */
+object NoAction : TriggerStrategy()
+
+/**
+ * [IActionObjectPool] will commit action objects immediately once [BaseActionObject] has been put into.
+ */
 object Immediate : TriggerStrategy()
 
+/**
+ * [IActionObjectPool] will commit action objects when there are at least [count] items have been put into.
+ */
 class CountFromLast(val count: Int = LIKE_COUNT_ON_TRIGGER) : TriggerStrategy() {
 
     override fun toString(): String = "${super.toString()}(count=$count)"
 }
 
+/**
+ * [IActionObjectPool] will commit action objects when a specific [delay] will have been passed since
+ * the moment the first action object with such strategy has been put into.
+ */
 class DelayFromLast(val delay: Long = LIKE_DELAY_ON_TRIGGER) : TriggerStrategy() {  // in seconds
 
     override fun toString(): String = "${super.toString()}(delay=$delay)"
--- core/domain/src/main/kotlin/com/ringoid/domain/model/actions/ReadMessageActionObject.kt
+++ core/domain/src/main/kotlin/com/ringoid/domain/model/actions/ReadMessageActionObject.kt
@@ -3,14 +3,16 @@ package com.ringoid.domain.model.actions
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.ringoid.domain.action_storage.Immediate
+import com.ringoid.domain.action_storage.TriggerStrategy
 import com.ringoid.domain.model.actions.ActionObject.Companion.ACTION_TYPE_MESSAGE_READ
 
 class ReadMessageActionObject(
     @Expose @SerializedName(COLUMN_MESSAGE_ID) val messageId: String,
     @Expose @SerializedName(COLUMN_PEER_ID) val peerId: String,
-    actionTime: Long = System.currentTimeMillis())
+    actionTime: Long = System.currentTimeMillis(),
+    triggerStrategies: List<TriggerStrategy> = listOf(Immediate))  // TODO: choose optimal strategy
     : OriginActionObject(actionTime = actionTime, actionType = ACTION_TYPE_MESSAGE_READ,
-                         triggerStrategies = listOf(Immediate)) {
+                         triggerStrategies = triggerStrategies) {
 
     companion object {
         const val COLUMN_MESSAGE_ID = "msgId"
