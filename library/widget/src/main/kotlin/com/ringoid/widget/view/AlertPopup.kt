5c4cce8843e1d916e0bb8b136056edf47835944d
Maxim Alov <orcchg@yandex.ru>
2019-09-25 16:42:54 +0300

    #938: Add widget to show alerts

---
+++ library/widget/src/main/kotlin/com/ringoid/widget/view/AlertPopup.kt
@@ -0,0 +1,80 @@
+package com.ringoid.widget.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.ringoid.utility.getAttributeColor
+import com.ringoid.widget.R
+import kotlinx.android.synthetic.main.widget_alert_popup_layout.view.*
+
+class AlertPopup : ConstraintLayout {
+
+    constructor(context: Context): this(context, null)
+
+    constructor(context: Context, attributes: AttributeSet?): this(context, attributes, 0)
+
+    constructor(context: Context, attributes: AttributeSet?, defStyleAttr: Int): super(context, attributes, defStyleAttr) {
+        init(context, attributes, defStyleAttr)
+    }
+
+    private fun init(context: Context, attributes: AttributeSet?, defStyleAttr: Int) {
+        LayoutInflater.from(context).inflate(R.layout.widget_alert_popup_layout, this, true)
+
+        with (context.obtainStyledAttributes(attributes, R.styleable.AlertPopup, defStyleAttr, R.style.AlertPopup)) {
+            val defaultBg = context.getAttributeColor(R.attr.refColorBg)
+            val defaultTextPrimaryColor = context.getAttributeColor(R.attr.refTextColorPrimary)
+
+            setBackgroundResource(getResourceId(R.styleable.AlertPopup_alert_bg, defaultBg))
+            setActionTextColorRes(colorResId = getResourceId(R.styleable.AlertPopup_alert_action_text_color, defaultTextPrimaryColor))
+            setDescriptionTextColorRes(colorResId = getResourceId(R.styleable.AlertPopup_alert_description_text_color, defaultTextPrimaryColor))
+            getResourceId(R.styleable.AlertPopup_alert_action_text, 0)
+                .takeIf { it != 0 }?.let { setActionText(resId = it) }
+                ?: run { setActionText(text = getString(R.styleable.AlertPopup_alert_action_text)) }
+            getResourceId(R.styleable.AlertPopup_alert_description_text, 0)
+                .takeIf { it != 0 }?.let { setDescriptionText(resId = it) }
+                ?: run { setDescriptionText(text = getString(R.styleable.AlertPopup_alert_description_text)) }
+            setIcon(resId = getResourceId(R.styleable.AlertPopup_alert_icon, 0))
+            setHideIcon(resId = getResourceId(R.styleable.AlertPopup_alert_icon_hide, 0))
+
+            recycle()
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------
+    private fun setActionText(@StringRes resId: Int) {
+        btn_action.setText(resId)
+    }
+
+    private fun setActionText(text: String?) {
+        btn_action.text = text ?: ""
+    }
+
+    private fun setActionTextColorRes(@ColorRes colorResId: Int) {
+        btn_action.setTextColor(ContextCompat.getColor(context, colorResId))
+    }
+
+    private fun setDescriptionText(@StringRes resId: Int) {
+        tv_description.setText(resId)
+    }
+
+    private fun setDescriptionText(text: String?) {
+        tv_description.text = text ?: ""
+    }
+
+    private fun setDescriptionTextColorRes(@ColorRes colorResId: Int) {
+        tv_description.setTextColor(ContextCompat.getColor(context, colorResId))
+    }
+
+    private fun setIcon(@DrawableRes resId: Int) {
+        iv_icon.setImageResource(resId)
+    }
+
+    private fun setHideIcon(@DrawableRes resId: Int) {
+        iv_hide_icon.setImageResource(resId)
+    }
+}
