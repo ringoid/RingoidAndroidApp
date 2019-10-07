eb88f96647a49e87105bf44b497484e21d102b7b
Maxim Alov <orcchg@yandex.ru>
2019-10-07 15:52:23 +0300

    Mark class for debug only purposes

--- core/domain/src/main/kotlin/com/ringoid/domain/interactor/actions/CountActionObjectsCachedInPoolUseCase.kt
+++ core/domain/src/main/kotlin/com/ringoid/domain/interactor/actions/CountActionObjectsCachedInPoolUseCase.kt
@@ -5,9 +5,11 @@ import com.ringoid.domain.executor.UseCaseThreadExecutor
 import com.ringoid.domain.interactor.base.Params
 import com.ringoid.domain.interactor.base.SingleUseCase
 import com.ringoid.domain.repository.actions.IActionObjectRepository
+import com.ringoid.utility.DebugOnly
 import io.reactivex.Single
 import javax.inject.Inject
 
+@DebugOnly
 class CountActionObjectsCachedInPoolUseCase @Inject constructor(private val repository: IActionObjectRepository,
     threadExecutor: UseCaseThreadExecutor, postExecutor: UseCasePostExecutor)
     : SingleUseCase<Int>(threadExecutor, postExecutor) {
