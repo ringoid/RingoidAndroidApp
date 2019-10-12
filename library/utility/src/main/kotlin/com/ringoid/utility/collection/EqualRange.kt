f6ff14ac9519901e2c486aab90ea50b6b36641dd
Maxim Alov <orcchg@yandex.ru>
2019-10-12 13:05:48 +0300

    Fix inconsistency between size and range for EqualRange on copyWith() when discarding items near footer, since negative 'delta' value should be added to size to make it less, not substracted, since negative substraction is a positive addition

--- library/utility/src/main/kotlin/com/ringoid/utility/collection/EqualRange.kt
+++ library/utility/src/main/kotlin/com/ringoid/utility/collection/EqualRange.kt
@@ -24,7 +24,7 @@ class EqualRange<T>(val from: Int, val to: Int, items: List<T>) : ArrayList<T>(i
         return when {
             delta == 0 -> EqualRange(from, to, items)  // range size not changed, some items could be changed
             delta > 0 -> EqualRange(from, to + delta, items)  // range's become longer
-            delta < 0 -> EqualRange(from, to - delta, items)  // range's become shorter
+            delta < 0 -> EqualRange(from, to + delta, items)  // range's become shorter
             else -> EqualRange(from, to, items)  // unreachable case
         }
     }
