e803f193bcff7edd5b113e27c15132cc33e67cab
Maxim Alov <orcchg@yandex.ru>
2019-10-15 18:04:25 +0300

    Don't report when getChat() network request has been canceled, since this is usually the case when Chat screen is closed during next polling, so this is expected behavir

--- library/data/src/main/kotlin/com/ringoid/data/remote/network/interceptor_utils.kt
+++ library/data/src/main/kotlin/com/ringoid/data/remote/network/interceptor_utils.kt
@@ -4,8 +4,14 @@ import com.ringoid.debug.DebugLogUtil
 import com.ringoid.report.log.Report
 import java.io.IOException
 
+fun Throwable.isCanceledNetworkRequest(): Boolean =
+    this is IOException && message?.contains("Canceled") == true
+
 fun Throwable.reportNetworkInterceptionError(requestUrl: String, from: String) {
     DebugLogUtil.e(this, "Chain failed [$requestUrl]: $message")
+    if (isCanceledNetworkRequest() && requestUrl.contains("/feeds/chat")) {
+        return  // don't report canceled /feeds/chat requests
+    }
     Report.capture(this, "Chain proceed has failed",
         extras = listOf("url" to requestUrl, "cause" to (message ?: ""),
                         "inner cause" to "${cause?.javaClass?.simpleName}",
